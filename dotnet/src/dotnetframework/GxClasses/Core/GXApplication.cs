c3c535cfbff377c6ea42264093a018e751b7a4d3
cmurialdo <cmurialdo@genexus.com>
2020-01-22 16:47:40 -0300

    Guid.NewGuid is not guaranteed to not equal Guid.Empty

--- dotnet/src/dotnetframework/GxClasses/Core/GXApplication.cs
+++ dotnet/src/dotnetframework/GxClasses/Core/GXApplication.cs
@@ -3550,7 +3550,12 @@ namespace GeneXus.Application
 		}
 		public string FileFromBase64(string b64)
 		{
-			string tmpFileName = Guid.NewGuid().ToString();
+			Guid tmpGuid = Guid.NewGuid();
+			string tmpFileName;
+			if (tmpGuid == Guid.Empty)//Guid.NewGuid is not guaranteed to not equal Guid.Empty
+				tmpFileName = Math.Truncate(NumberUtil.Random() * 9999).ToString();
+			else
+				tmpFileName = tmpGuid.ToString();
 			string filePath = Path.Combine(Preferences.getTMP_MEDIA_PATH(), "Blob" + tmpFileName);
 			GxFile auxFile = new GxFile(GetPhysicalPath(), filePath, GxFileType.Private);
 			auxFile.FromBase64(b64);
@@ -3567,7 +3572,12 @@ namespace GeneXus.Application
 		}
 		public string FileFromByteArray(byte[] bArray)
 		{
-			string tmpFileName = Guid.NewGuid().ToString();
+			Guid tmpGuid = Guid.NewGuid();
+			string tmpFileName;
+			if (tmpGuid == Guid.Empty) //Guid.NewGuid is not guaranteed to not equal Guid.Empty
+				tmpFileName = Math.Truncate(NumberUtil.Random() * 9999).ToString();
+			else
+				tmpFileName = tmpGuid.ToString();
 			string filePath = Path.Combine(Preferences.getTMP_MEDIA_PATH(), "Blob" + tmpFileName);
 			GxFile auxFile = new GxFile(GetPhysicalPath(), filePath, GxFileType.Private);
 			auxFile.FromByteArray(bArray);
