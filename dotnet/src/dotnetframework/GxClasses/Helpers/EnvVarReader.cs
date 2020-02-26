54d9b454e1453957d09fd0619dbb7adac3750ff4
Seba Gomez <sebastiangomezcorrea@gmail.com>
2020-02-26 10:47:25 -0300

    The CloudService properties can now be overridden by environment variables Also instead of trying to read the environment value on access request, it gets read (and cached) when the application starts. With the old mechanismo we were trying to access the environment variables every time a property value was being asked.

---
+++ dotnet/src/dotnetframework/GxClasses/Helpers/EnvVarReader.cs
@@ -0,0 +1,53 @@
+using System;
+using GeneXus;
+using log4net;
+
+namespace GxClasses.Helpers
+{
+	internal class EnvVarReader
+	{
+		private static readonly ILog log = LogManager.GetLogger(typeof(EnvVarReader));
+
+		private const string ENVVAR_PREFIX = "GX_";
+		public static bool GetEnvironmentValue(string name, out string value)
+		{
+			try
+			{
+				value = Environment.GetEnvironmentVariable($"{ENVVAR_PREFIX}{name.ToUpper()}");
+				if (!string.IsNullOrEmpty(value))
+					return true;
+
+				return false;
+			}
+			catch
+			{
+				value = string.Empty;
+				return false;
+			}
+		}
+
+		public static bool GetEnvironmentValue(string serviceType, string serviceName, string propertyName, out string value)
+		{
+			try
+			{
+				string envVarName = $"{ENVVAR_PREFIX}{serviceType.ToUpper()}_{propertyName.ToUpper()}";
+				value = Environment.GetEnvironmentVariable(envVarName);
+				if (!string.IsNullOrEmpty(value))
+					return true;
+
+				envVarName = $"{ENVVAR_PREFIX}{serviceType.ToUpper()}__{serviceName.ToUpper()}_{propertyName.ToUpper()}";
+				value = Environment.GetEnvironmentVariable(envVarName);
+
+				if (!string.IsNullOrEmpty(value))
+					return true;
+
+				return false;
+			}
+			catch
+			{
+				value = string.Empty;
+				return false;
+			}
+		}
+	}
+}
