4b3bf48865bbcbc6aaf31fd411ebca4beb11d1f2
builder <builder@genexus.com>
2020-03-26 12:43:41 +0000

    Cherry pick branch 'genexuslabs:UniqueAssemblyIdForJavascriptUrl' into beta

--- dotnet/src/dotnetframework/GxClasses/Helpers/AssemblyHelper.cs
+++ dotnet/src/dotnetframework/GxClasses/Helpers/AssemblyHelper.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace GeneXus.Helpers
@@ -7,8 +10,33 @@ namespace GeneXus.Helpers
 	{
 		public static string GetUniqueId()
 		{
-			var datetime = new FileInfo(Assembly.GetCallingAssembly().Location).LastWriteTimeUtc;
+			Assembly assembly = Assembly.GetCallingAssembly();
+			DateTime datetime = GetBuildDate(assembly);
+			if (datetime==null)
+				datetime = new FileInfo(assembly.Location).LastWriteTimeUtc;
+
 			return datetime.Ticks.ToString();
+		}
+		private static DateTime GetBuildDate(Assembly assembly)
+		{
+			const string BuildVersionMetadataSuffix = ".";
+
+			var attribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+			if (attribute?.InformationalVersion != null)
+			{
+				var value = attribute.InformationalVersion;
+				var index = value.IndexOf(BuildVersionMetadataSuffix);
+				if (index > 0)
+				{
+					value = value.Substring(0, index);
+				}
+				if (DateTime.TryParseExact(value, "yyyyMMddHHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None, out var result))
+				{
+					return result;
+				}
+			}
+
+			return default;
 		}
 	}
 }
