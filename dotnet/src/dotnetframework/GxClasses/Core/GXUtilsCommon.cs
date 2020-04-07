d0f8e4567edb65971928b82c6ff06bbc566ac6eb
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2020-04-07 20:24:56 -0300

    ExitCode of failed reorganization was not -1. (#101)
    Co-authored-by: claudia <cmurialdo@gmail.com>

--- dotnet/src/dotnetframework/GxClasses/Core/GXUtilsCommon.cs
+++ dotnet/src/dotnetframework/GxClasses/Core/GXUtilsCommon.cs
@@ -4721,7 +4721,7 @@ namespace GeneXus.Utils
 					{
 						if (GXProcessHelper.ProcessFactory != null)
 						{
-							GXProcessHelper.ProcessFactory.GetProcessHelper().ExecProcess(RunAsX86Path, args, basePath, executable, dataReceived);
+							exitCode = GXProcessHelper.ProcessFactory.GetProcessHelper().ExecProcess(RunAsX86Path, args, basePath, executable, dataReceived);
 							return true;
 						}
 						else
