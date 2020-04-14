1436e9a8e4cfbbafd34a711ecb52324edd5c228c
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2020-04-14 17:13:17 -0300

    Change public key token tool (#110)
    * Add tool for patch assemblies when changing strong name key of GxClasses.dll
    
    * Improve README.md
    
    * Add to readme the steps to replace an standard assembly.
    
    * Fix verbose argument.
    
    * Complete table of modules
    
    * Unify project and assembly name
    
    Co-authored-by: claudia <cmurialdo@gmail.com>
    (cherry picked from commit 2a35547b3d8ae6f7a59cfcf284a25db1f88ee6ca)

--- tools/updateassemblyreference/Properties/AssemblyInfo.cs
+++ tools/updateassemblyreference/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChangePublicKeyToken")]
+[assembly: AssemblyTitle("UpdateAssemblyReference")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChangePublicKeyToken")]
+[assembly: AssemblyProduct("UpdateAssemblyReference")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
