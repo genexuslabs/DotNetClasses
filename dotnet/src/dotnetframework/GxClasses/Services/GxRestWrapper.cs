8ef39e73f722d16c24cc782c948fd911072af5bb
builder <builder@genexus.com>
2020-04-14 22:37:52 +0000

    Cherry pick branch 'genexuslabs:FixErrorBCRest' into beta

--- dotnet/src/dotnetframework/GxClasses/Services/GxRestWrapper.cs
+++ dotnet/src/dotnetframework/GxClasses/Services/GxRestWrapper.cs
@@ -53,7 +53,10 @@ namespace GeneXus.Application
 			AddHeader("Content-type", "application/json; charset=utf-8"); //MediaTypesNames.ApplicationJson);
 			RunAsMain = true;
 		}
-		
+		protected virtual GXBaseObject Worker
+		{
+			get { return _procWorker; }
+		}
 		public virtual void Cleanup()
 		{
 			if (RunAsMain)
@@ -260,7 +263,7 @@ namespace GeneXus.Application
 		}
 		public bool IsAuthenticated()
 		{
-			return IsAuthenticated(_procWorker.IntegratedSecurityLevel2, _procWorker.IntegratedSecurityEnabled2, _procWorker.ExecutePermissionPrefix2);
+			return IsAuthenticated(Worker.IntegratedSecurityLevel2, Worker.IntegratedSecurityEnabled2, Worker.ExecutePermissionPrefix2);
 		}
 		private bool IsAuthenticated(GAMSecurityLevel objIntegratedSecurityLevel, bool objIntegratedSecurityEnabled, string objPermissionPrefix)
 		{
