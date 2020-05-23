14c295f4b3e2a0787c041a360339f0dbc15d65cf
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2020-05-23 12:31:49 -0300

    Add support for user control 2.0 in dotnetcore. (#148)
    * Add support for user control 2.0 in dotnetcore.
    
    * Remove unnecessary Debugger.Launch line
    
    Co-authored-by: claudia <cmurialdo@gmail.com>

--- dotnet/src/dotnetframework/GxClasses/UserControls/UserControlGenerator.cs
+++ dotnet/src/dotnetframework/GxClasses/UserControls/UserControlGenerator.cs
@@ -5,14 +5,23 @@ using System.Reflection;
 using System.Text;
 using GeneXus.Application;
 using GeneXus.Utils;
+#if NETCORE
+using Stubble.Core;
+using Stubble.Core.Builders;
+#else
 using Nustache.Core;
+#endif
 
 namespace GeneXus.UserControls
 {
 	public class UserControlGenerator
 	{
 		private string m_Type;
+#if NETCORE
+		private string m_Template;
+#else
 		private Template m_Template;
+#endif
 		public UserControlGenerator(string ucType)
 		{
 			m_Type = ucType;
@@ -26,11 +35,20 @@ namespace GeneXus.UserControls
 			return Path.Combine(GxContext.StaticPhysicalPath(), $"gxusercontrols\\{type}.view");
 		}
 
-	
 		internal string Render(string internalName, GxDictionary propbag)
 		{
 			if (!File.Exists(GetTemplateAction(m_Type)))
 				return String.Empty;
+#if NETCORE
+			var stubble = new StubbleBuilder().Build();
+			string output = string.Empty;
+			if (GetTemplateDateTime() > LastRenderTime || m_Template == null)
+			{
+				m_Template = File.ReadAllText(GetTemplateFile(m_Type));
+				LastRenderTime = DateTime.UtcNow;
+			}
+			return stubble.Render(m_Template, propbag);
+#else
 			Encoders.HtmlEncode = (input) => input;  
 
 			if (GetTemplateDateTime() > LastRenderTime || m_Template == null)
@@ -45,10 +63,11 @@ namespace GeneXus.UserControls
 			StringBuilder sb = new StringBuilder();
 			m_Template.Render(propbag, new StringWriter(sb), null);
 			return sb.ToString();
+
+#endif
 		}
 
-
-		private DateTime GetTemplateDateTime()
+	private DateTime GetTemplateDateTime()
 		{
 			return File.GetLastWriteTimeUtc(GetTemplateAction(m_Type));
 		}
