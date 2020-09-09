74a32c4077dce3cd676c8cf8f80c252017f601ca
builder <builder@genexus.com>
2020-09-09 21:13:00 +0000

    Cherry pick branch 'genexuslabs:NamedParametersAndEncrypt' into beta

--- dotnet/src/dotnetframework/GxClasses/Middleware/GXHttp.cs
+++ dotnet/src/dotnetframework/GxClasses/Middleware/GXHttp.cs
@@ -2155,7 +2155,7 @@ namespace GeneXus.Http
 			if (!string.IsNullOrEmpty(value))
 			{
 				value = GxContext.RemoveInternalSuffixes(value).TrimStart('?');
-				useOldQueryStringFormat = !value.Contains("=");
+				useOldQueryStringFormat = !(Preferences.UseNamedParameters && value.Contains("="));
 				if (!string.IsNullOrEmpty(value))
 				{
 					string[] elements = useOldQueryStringFormat ? value.Split(',') : value.Split('&');
