5e79de8870fb93d1de06f669ab8df210132a73cf
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2020-10-06 19:41:08 -0300

    Fix Link function for absolute and empty urls (#244)
    * Fix Link function for absolute and empty urls
    
    * Some Cookie headers are doubly scaped in .netcore.
    
    Co-authored-by: claudia <cmurialdo@gmail.com>

--- dotnet/src/dotnetframework/GxClasses/Model/URLRouter.cs
+++ dotnet/src/dotnetframework/GxClasses/Model/URLRouter.cs
@@ -29,12 +29,12 @@ namespace GeneXus.Application
 
 			string[] urlQueryString = key.Split('?');
 			string query = urlQueryString.Length > 1 ? urlQueryString[1] : string.Empty;
-			string path = urlQueryString[0].ToLower();
+			string path = urlQueryString[0];
 
 			string[] parameterValues= string.IsNullOrEmpty(query) ? parms : HttpHelper.GetParameterValues(query);
 
 			string routerKey;
-			bool rewriteMatch = routerList.TryGetValue(path, out routerKey);
+			bool rewriteMatch = routerList.TryGetValue(NormalizedUrlObjectName(path), out routerKey);
 			if (rewriteMatch)
 			{
 				path = string.Format(routerKey, parameterValues);
@@ -47,18 +47,20 @@ namespace GeneXus.Application
 				if (PatternHasParameters(routerKey))
 					result = $"{basePath}{path}";
 				else
-					result = $"{basePath}{path}{ConvertParmsToQueryString(parms, parmsName, path)}";
+					result = $"{basePath}{path}{ConvertParmsToQueryString(parms, parmsName)}";
 			}
 			else if (parms.Length > 0)
 			{
-				result = $"{basePath}{path}{ConvertParmsToQueryString(parms, parmsName, path)}";
+				result = $"{basePath}{path}{ConvertParmsToQueryString(parms, parmsName)}";
 			}
 			else if (!string.IsNullOrEmpty(query))
 			{
 				result = $"{basePath}{path}?{query}";
 			}
-			else
+			else if (!string.IsNullOrEmpty(path))
 				result = $"{basePath}{path}";
+			else
+				result = path;
 
 			return result;
 		}
@@ -77,7 +79,7 @@ namespace GeneXus.Application
 		{
 			return !string.IsNullOrEmpty(routerKey) && routerKey.Contains("{0}");
 		}
-		private static string ConvertParmsToQueryString(string[] parms, string[] parmsName, string routerRule)
+		private static string ConvertParmsToQueryString(string[] parms, string[] parmsName)
 		{
 			if (parms.Length == 0)
 				return string.Empty;
@@ -100,7 +102,10 @@ namespace GeneXus.Application
 			}
 			return queryString.ToString();
 		}
-
+		private static string NormalizedUrlObjectName(string objectName)
+		{
+			return objectName.ToLower();
+		}
 		private static void Load(string resourcePattern)
 		{
 			string[] files = Directory.GetFiles(GxContext.StaticPhysicalPath(), resourcePattern);
@@ -114,7 +119,7 @@ namespace GeneXus.Application
 						string objectName = map[0];
 						string url = map[1];
 						url = url.Replace(@"\=", "=").Replace(@"\\", @"\");
-						routerList[$"{objectName.ToLower()}.aspx"] = url;
+						routerList[$"{NormalizedUrlObjectName(objectName)}.aspx"] = url;
 					}
 				}
 			}
