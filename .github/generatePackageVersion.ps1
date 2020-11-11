83b53591e68ada9646f0039358406c4f3d35d85c
jechague <jechague@outlook.com>
2020-11-11 14:34:52 -0300

    Refactor build workflows (#249)
    * Use workflow_dispatch to build the master and beta branches
    
    * Update build.props for new build workflow
    
    * Add a version generating script and required parameters for the workflow dispatch
    
    * Setup GPR using the Setup-Nuget action
    
    * Update main branch name on build repo
    
    * Add codeowners file
    
    * Use a team in CODEOWNERS
    
    * Update codeowners team name.
    
    * Trigger workflow for builds in the master, beta and release branches so that external users can see the build status of any commit
    
    Co-authored-by: aberta <aberta@genexus.com>
    (cherry picked from commit b31169f7edb19d645648b5622830296eb47590aa)
    (cherry picked from commit cc5fb3740af2e38e9a1b2bfa393ad665818e2bb1)

---
+++ .github/generatePackageVersion.ps1
@@ -0,0 +1,13 @@
+$GetFileVersionOutput = dotnet msbuild $PSScriptRoot/../dotnet/Directory.Build.props /t:GetFileVersionForPackage
+"$GetFileVersionOutput" -match "(?<=FileVersion:)(.*)" > $null
+$GetFileVersionOutput = $Matches[0]
+
+$NuGetPackageVersion = $GetFileVersionOutput
+if ([System.Convert]::ToBoolean($Env:IsPrerelease)) {
+  $IsMaster = $Env:GIT_REF -match 'master$'
+  $VersionTag =  @("trunk", "stable")[$IsMaster]
+  $Timestamp = (Get-Date).ToString("yyyyMMddHHmmss")
+  $NuGetPackageVersion = $NuGetPackageVersion + "-" + $VersionTag + "." + $Timestamp
+}
+
+return $NuGetPackageVersion
