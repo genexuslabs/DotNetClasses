1cf78d1f45a60350bda350541fb65f53af4dddac
builder <builder@genexus.com>
2020-11-20 17:51:59 +0000

    Cherry pick branch 'genexuslabs:MySQLConnectorFix' into beta

--- dotnet/src/dotnetframework/GxClasses/Data/GXDataMysqlConnector.cs
+++ dotnet/src/dotnetframework/GxClasses/Data/GXDataMysqlConnector.cs
@@ -278,9 +278,9 @@ namespace GeneXus.Data
 			GXLogging.Debug(log, "ProcessError: dbmsErrorCode=" + dbmsErrorCode + ", emsg '" + emsg + "'");
 			switch (dbmsErrorCode)
 			{
-				case 2006://MySQL server has gone away
 				case 1042: //SSL Authentication Error
-					if (con != null && m_FailedConnections < MAX_TRIES)//Retry if it is an Open operation.
+				case 2006://MySQL server has gone away
+					if ((dbmsErrorCode == 1042 && emsg.Contains("SSL") || dbmsErrorCode==2006) && (con != null && m_FailedConnections < MAX_TRIES))//Retry if it is an Open operation.
 					{
 						try
 						{
