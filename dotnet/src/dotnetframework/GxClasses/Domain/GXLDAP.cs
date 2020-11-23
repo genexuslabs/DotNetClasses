28793673164112bf65b6cd2a6a60892630d4351d
builder <builder@genexus.com>
2020-11-23 22:41:08 +0000

    Cherry pick branch 'genexuslabs:NetCoreLDAPLinux' into beta

--- dotnet/src/dotnetframework/GxClasses/Domain/GXLDAP.cs
+++ dotnet/src/dotnetframework/GxClasses/Domain/GXLDAP.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections;
 using System.DirectoryServices;
 using System.Text;
 using log4net;
+#if NETCORE
+using Novell.Directory.Ldap;
+#endif
 
 namespace GeneXus.Utils
 {
@@ -16,6 +20,9 @@ namespace GeneXus.Utils
 		string _password;
         int _secure;
 		DirectoryEntry _entry;
+#if NETCORE
+		LdapConnection _conn;
+#endif
 		public GXLDAPClient()
 		{
 			_server = "localhost";
@@ -54,33 +61,103 @@ namespace GeneXus.Utils
             get { return _secure; }
             set { _secure = value; }
         }
-		public short Connect()
-		{
-			if (_entry != null)
+#if NETCORE
+		internal int getLdapConnectionPort() {
+			int port = 0;
+			if (_port != 0)
 			{
-				_entry.Close();
-				_entry = null;
+				port = _port;
 			}
-			_entry = new DirectoryEntry("LDAP://"+getPath(), _user, _password, getAuthentication());
+			else
+			{
+				if (_secure == 1)
+				{
+					port = LdapConnection.DefaultSslPort;
+				}
+				else
+				{
+					port = LdapConnection.DefaultPort;
+				}
+			}
+			return port;
+		}
+		internal short NovellConnect()
+		{
 			try
 			{
-				if (_entry.NativeObject != null)
-					return 1;
-				else
-					return 0;
+				if (_conn != null)
+				{
+					_conn.Disconnect();
+					_conn = null;
+				}
+				_conn = new LdapConnection();
+				_conn.SecureSocketLayer = (_secure == 1);
+				_conn.Connect(_server, getLdapConnectionPort());
+				_conn.Bind(_user, _password);
+				return (short)((_conn.Connected) ? 1 : 0);
 			}
-			catch (Exception ex) //DirectoryServicesCOMException
+			catch (Exception ex)
 			{
 				GXLogging.Error(log, "Connect Method Error.", ex);
 				return 0;
 			}
 		}
+		void NovellDisconnect()
+		{
+			if (_conn != null)
+			{
+				_conn.Disconnect();
+				_conn = null;
+			}
+		}
+#endif
+		public short Connect()
+		{
+#if NETCORE
+
+			if (!GXUtil.IsWindowsPlatform)
+			{
+				return NovellConnect();
+			}
+			else
+#endif
+			{
+
+				if (_entry != null)
+				{
+					_entry.Close();
+					_entry = null;
+				}
+				_entry = new DirectoryEntry("LDAP://" + getPath(), _user, _password, getAuthentication());
+				try
+				{
+					if (_entry.NativeObject != null)
+						return 1;
+					else
+						return 0;
+				}
+				catch (Exception ex) //DirectoryServicesCOMException
+				{
+					GXLogging.Error(log, "Connect Method Error.", ex);
+					return 0;
+				}
+			}
+		}
 		public void Disconnect()
 		{
-			if (_entry != null)
+#if NETCORE
+			if (GXUtil.IsWindowsPlatform)
 			{
-				_entry.Close();
-				_entry = null;
+				NovellDisconnect();
+			}
+			else
+#endif
+			{
+				if (_entry != null)
+				{
+					_entry.Close();
+					_entry = null;
+				}
 			}
 		}
 		string getPath()
@@ -104,41 +181,79 @@ namespace GeneXus.Utils
 
 			return AuthenticationTypes.None;
 		}
-		public GxSimpleCollection<string> GetAttribute( string name, string context, GXProperties atts)
+		public GxSimpleCollection<string> GetAttribute(string name, string context, GXProperties atts)
 		{
-			if (_entry != null)
-			{
-				_entry.Close();
-				_entry = null;
-			}
-			string context1;
-			if (context.Trim().Length == 0)
-				context1 = "";
-			else 
-				context1 = "/"+context;
-			AuthenticationTypes at = getAuthentication();
-			_entry = new DirectoryEntry("LDAP://"+getPath()+context1, _user, _password, at);
 			string filter = "";
 			if (atts.Count == 0)
-				filter = "("+name+"=*)";
+				filter = "(" + name + "=*)";
 			else
 			{
-				for( int i=0;i<atts.Count;i++)
-					filter+= "("+atts.GetKey(i).Trim()+"="+atts[i].Trim()+")";
+				for (int i = 0; i < atts.Count; i++)
+					filter += "(" + atts.GetKey(i).Trim() + "=" + atts[i].Trim() + ")";
 				if (atts.Count > 1)
-					filter = "(&"+filter+")";
+					filter = "(&" + filter + ")";
 			}
-			DirectorySearcher ds = new DirectorySearcher(_entry, filter, new string[] {name});
 			GxSimpleCollection<string> sc = new GxSimpleCollection<string>();
+
 			try
 			{
-				foreach (System.DirectoryServices.SearchResult result in ds.FindAll())
+#if NETCORE
+				if (!GXUtil.IsWindowsPlatform)
 				{
-					PropertyValueCollection values = (PropertyValueCollection)(result.GetDirectoryEntry().Properties[name]);
-					StringBuilder sb = new StringBuilder();
-					for (int i = 0; i < values.Count; i++)
-						sb.Append(values[i].ToString() + " ");
-					sc.Add(sb.ToString());
+					NovellConnect();
+
+					string searchBase = context;
+					int searchScope = LdapConnection.ScopeSub;
+					string searchFilter = filter;
+					ILdapSearchResults lsc = _conn.Search(searchBase, searchScope, searchFilter, new string[] { name }, false);
+
+					while (lsc.HasMore())
+					{
+						LdapEntry nextEntry = null;
+						try
+						{
+							nextEntry = lsc.Next();
+						}
+						catch (LdapException)
+						{
+							continue;
+						}
+						LdapAttributeSet attributeSet = nextEntry.GetAttributeSet();
+						IEnumerator ienum = attributeSet.GetEnumerator();
+						StringBuilder sb = new StringBuilder();
+						while (ienum.MoveNext())
+						{
+							LdapAttribute attribute = (LdapAttribute)ienum.Current;
+							string attributeVal = attribute.StringValue;
+							sb.Append(attributeVal + " ");
+						}
+						sc.Add(sb.ToString() + " ");
+					}
+				}
+				else
+#endif
+				{
+					if (_entry != null)
+					{
+						_entry.Close();
+						_entry = null;
+					}
+					string context1;
+					if (context.Trim().Length == 0)
+						context1 = "";
+					else
+						context1 = "/" + context;
+					AuthenticationTypes at = getAuthentication();
+					_entry = new DirectoryEntry("LDAP://" + getPath() + context1, _user, _password, at);
+					DirectorySearcher ds = new DirectorySearcher(_entry, filter, new string[] { name });
+					foreach (SearchResult result in ds.FindAll())
+					{
+						PropertyValueCollection values = (PropertyValueCollection)(result.GetDirectoryEntry().Properties[name]);
+						StringBuilder sb = new StringBuilder();
+						for (int i = 0; i < values.Count; i++)
+							sb.Append(values[i].ToString() + " ");
+						sc.Add(sb.ToString());
+					}
 				}
 			}
 			catch (Exception ex)
@@ -146,7 +261,6 @@ namespace GeneXus.Utils
 				GXLogging.Error(log, "GetAttribute Method Error.", ex);
 			}
 			return sc;
-
 		}
 	}
 
