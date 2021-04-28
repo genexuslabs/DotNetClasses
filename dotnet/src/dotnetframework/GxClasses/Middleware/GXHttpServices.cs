710b40fcbbcb3786828b2a4f791c417f3d846a32
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2021-04-28 08:18:19 -0300

    Make each context delete Temporary files created by itself and not the ones belonging to another context. (#336)
    That is the case when a proc command line creates temporary files, at the same time that a new proc runs in a new utl which also creates temporary blobs => in that case the context destructor of new utl proc might destroy temporary files of the first procedure.

--- dotnet/src/dotnetframework/GxClasses/Middleware/GXHttpServices.cs
+++ dotnet/src/dotnetframework/GxClasses/Middleware/GXHttpServices.cs
@@ -424,7 +424,7 @@ namespace GeneXus.Http
 							thumbnailUrl = url,
 							path = fileToken
 						});
-						GxUploadHelper.CacheUploadFile(fileGuid, savedFileName, fName, ext, gxFile, localHttpContext);
+						GxUploadHelper.CacheUploadFile(fileGuid, savedFileName, fName, ext, gxFile, context);
 					}
 					UploadFilesResult result = new UploadFilesResult() { files = r };
 					var jsonObj = JSONHelper.Serialize(result);
@@ -476,7 +476,7 @@ namespace GeneXus.Http
 			HttpHelper.SetResponseStatus(localHttpContext, ((int)HttpStatusCode.Created).ToString(), string.Empty);
 			localHttpContext.Response.Write(obj.ToString());
 
-			GxUploadHelper.CacheUploadFile(fileGuid, savedFileName, fName, ext, file, localHttpContext);
+			GxUploadHelper.CacheUploadFile(fileGuid, savedFileName, fName, ext, file, context);
 		}
 		protected override bool IntegratedSecurityEnabled
 		{
