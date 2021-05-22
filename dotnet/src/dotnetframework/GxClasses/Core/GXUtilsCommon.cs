f5891c018e1b3e8009082dfbb4d65322bcf4aa4e
Claudia Murialdo <cmurialdo@genexus.com>
2021-05-21 23:40:02 -0300

    Compute the canonical path of multimedia files when building gxdbfile attribute.

--- dotnet/src/dotnetframework/GxClasses/Core/GXUtilsCommon.cs
+++ dotnet/src/dotnetframework/GxClasses/Core/GXUtilsCommon.cs
@@ -3687,6 +3687,19 @@ namespace GeneXus.Utils
 				return fileName;
 		}
 
+		internal static string SafeCombine(string basePath, string fileName)
+		{
+			string fullPath = Path.Combine(basePath, fileName);
+			string canonicalPath = Path.GetFullPath(fullPath);
+			if (canonicalPath.StartsWith(basePath, StringComparison.OrdinalIgnoreCase))
+			{
+				return canonicalPath;
+			}
+			else
+			{
+				return Path.Combine(FileUtil.GetFileName(fileName));
+			}
+		}
 	}
 
 	public class GXUtil
@@ -5261,7 +5274,7 @@ namespace GeneXus.Utils
 				string basePath = Path.Combine(Path.Combine(Preferences.getBLOB_PATH(), MultimediaDirectory));
 				try
 				{
-					GxFile file = new GxFile(string.Empty, Path.Combine(basePath, fileName), GxFileType.PublicAttribute);
+					GxFile file = new GxFile(string.Empty, PathUtil.SafeCombine(basePath, fileName), GxFileType.PublicAttribute);
 					return PathToUrl(file.GetURI(), absUrl, context);
 				}
 				catch (ArgumentException ex)
@@ -5275,16 +5288,7 @@ namespace GeneXus.Utils
 		public static string GenerateUri(string file, bool addToken, bool addScheme)
 		{
 			string name = GetFileName(file);
-			try
-			{
-				name = Uri.UnescapeDataString(name);
-			}
-			catch (Exception ex)
-			{
-				GXLogging.Warn(log, ex, "GenerateUri escape unnecesary ", name);
-			}
 			string type = GetFileType(file);
-
 
 			string scheme = addScheme ? $"{GXDbFile.Scheme}:" : string.Empty;
 
--- dotnet/test/DotNetUnitTest/FileIOTests.cs
+++ dotnet/test/DotNetUnitTest/FileIOTests.cs
@@ -50,5 +50,24 @@ namespace UnitTesting
 			Assert.True(uri.IsAbsoluteUri);
 			Assert.True(new FileInfo(uri.LocalPath).FullName == new FileInfo("PublicTempStorage/multimedia/myimg_8e1604b16eda43e59694f9aeb0b33e77.jpg").FullName);
 		}
+		[Fact]
+		public void GXDBFilePathTest()
+		{
+			string[] filesName = { "content%2f..%2f..%2f..%2fdocument.aspx","content%2f%2e%2e%2f%2e%2e%2f%2e%2e%2fdocument","content%5c%2e%2e%5c%2e%2e%5c%2e%2e%5cdocument",
+				"content%5c..%5c..%5c..%5cdocument.aspx","content%255c%252e%252e%255c%252e%252e%255c%252e%252e%255cdocument.aspx","content%255c..%255c..%255c..%255cdocument.aspx",
+				"content%c0%af..%c0%af..%c0%af..%c0%afdocument.aspx","content%c1%9c..%c1%9c..%c1%9c..%c1%9cdocument.aspx",
+				@"http://localhost/base/content%2f..%2f..%2f..%2fdocument.aspx"};
+
+			foreach (string fileName in filesName)
+			{
+				string newFileName = GXDbFile.GenerateUri(fileName, false, false);
+				string baseDir = Directory.GetCurrentDirectory();
+
+				string fullPath = Path.Combine(baseDir, newFileName);
+				string fullPath2 = Path.GetFullPath(fullPath);
+				bool isOK = fullPath2.StartsWith(baseDir, StringComparison.OrdinalIgnoreCase);
+				Assert.True(isOK);
+			}
+		}
 	}
 }
