42447cfec72ef0322c6b0a2651a9c1300897e055
builder <builder@genexus.com>
2021-06-02 14:16:59 -0300

    Cherry pick branch 'genexuslabs:AzureFunctionHandlers' into beta

---
+++ dotnet/src/extensions/AzureFunctionHandlers/test/AzureFunctionsTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace AzureFunctionsTest
+{
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
