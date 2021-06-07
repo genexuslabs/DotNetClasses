3a572bf1eca34b9945bae2f9bc4359c1df766604
sjuarezgx <sjuarez@genexus.com>
2021-06-07 12:14:53 -0300

    delete folder not used

--- dotnet/src/extensions/AzureFunctionHandlers/GeneXus.Deploy.AzureFunctions.Handlers/Helpers/FunctionExceptionType.cs
+++
@@ -1,8 +0,0 @@
-namespace GeneXus.Deploy.AzureFunctions.Handlers.Helpers
-{ 
-	public static class FunctionExceptionType
-	{
-		public const string SysRuntimeError = "[SystemRuntimeError]";
-		public const string AppError = "[ApplicationError]";
-	}
-}
