e26ea85fcad1783d3c6989a435a4d21eff7d1bd1
builder <builder@genexus.com>
2021-06-07 12:14:34 -0300

    Cherry pick branch 'genexuslabs:AzureFunctionHandlers' into beta

--- dotnet/src/extensions/AzureFunctionHandlers/GeneXus.Deploy.AzureFunctions.Handlers/Helpers/EventSourceType.cs
+++
@@ -1,9 +0,0 @@
-namespace GeneXus.Deploy.AzureFunctions.Handlers.Helpers
-{
-	public class EventSourceType
-	{
-		public const string QueueMessage = "QueueMessage";
-		public const string ServiceBusMessage = "ServiceBusMessage";
-		public const string Timer = "Timer";
-	}
-}
