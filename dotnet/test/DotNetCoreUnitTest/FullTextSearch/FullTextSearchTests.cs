2e643f6dba616e73edfde6177114642cacd03c68
Claudia Murialdo <cmurialdo@genexus.com>
2021-06-11 12:09:17 -0300

    Fix unit test. .Net test needs ntidy so cannot run on github actions.

--- dotnet/test/DotNetCoreUnitTest/FullTextSearch/FullTextSearchTests.cs
+++ dotnet/test/DotNetCoreUnitTest/FullTextSearch/FullTextSearchTests.cs
@@ -6,11 +6,7 @@ using System;
 using GeneXus.Cryptography;
 using GeneXus.Utils;
 using System.IO;
-#if NETCORE
 namespace xUnitTesting
-#else
-namespace UnitTesting
-#endif
 {
 	public class FullTextSearch
 	{
@@ -21,13 +17,6 @@ namespace UnitTesting
 			string html = File.ReadAllText(htmlFileName);
 			string text = DocumentHandler.GetText(htmlFileName, "html");
 			string prettyHTML = DocumentHandler.HTMLClean(html);
-#if NETCORE
-			File.WriteAllText("textNETCORE.txt", text);
-			File.WriteAllText("cleanhtmlNETCORE.html", prettyHTML);
-#else
-			File.WriteAllText("textNET.txt", text);
-			File.WriteAllText("cleanhtmlNET.html", prettyHTML);
-#endif
 			Assert.Contains("atencionalcliente@gmail.com", text, StringComparison.OrdinalIgnoreCase);
 			Assert.DoesNotContain("<html><html>", prettyHTML, StringComparison.OrdinalIgnoreCase);
 
