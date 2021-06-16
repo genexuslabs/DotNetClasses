a9f167a4d728aac1531711cdf782e386047d3ef9
Gonzalo <gonzalogallotti@gmail.com>
2021-06-16 10:19:46 -0300

    Remove commented code
    (cherry picked from commit ef2692d46807b442b0e8652680c4617407d3d551)

--- dotnet/src/dotnetframework/GxClasses/Storage/StorageFactory.cs
+++ dotnet/src/dotnetframework/GxClasses/Storage/StorageFactory.cs
@@ -5,22 +5,6 @@ namespace GeneXus.Storage
 	public class StorageFactory
 	{
 		const char QUESTION_MARK = '?';
-		/*public static ExternalProvider GetExternalProviderFromUrl(string url, out string objectName)
-		{
-			objectName = null;
-			ExternalProvider provider = ServiceFactory.GetExternalProvider();
-			if (provider != null)
-			{
-				if (provider.TryGetObjectNameFromURL(url, out objectName))
-				{
-					var questionMarkIndex = objectName.IndexOf(QUESTION_MARK);
-					objectName = questionMarkIndex >= 0 ? objectName.Substring(0, questionMarkIndex): objectName.Substring(0);
-					return provider;
-				}
-			}
-			return null;
-		}
-		*/
 		public static bool TryGetProviderObjectName(ExternalProvider provider, string objectNameOrUrl, out string providerObjectName)
 		{
 			providerObjectName = null;
