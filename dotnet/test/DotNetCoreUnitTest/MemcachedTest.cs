5368a8bb653db7cded4fb5733d576b9c7737bf88
Claudia Murialdo <cmurialdo@genexus.com>
2021-06-18 18:44:36 -0300

    Reorganize variables

--- dotnet/test/DotNetCoreUnitTest/MemcachedTest.cs
+++ dotnet/test/DotNetCoreUnitTest/MemcachedTest.cs
@@ -12,15 +12,15 @@ namespace xUnitTesting
 		//[Fact]
 		public void TestCache()
 		{
+			string value;
+			List<string> keys = new List<string>();
+			List<string> values = new List<string>();
 			Memcached cache = new Memcached();
 			cache.Set<string>("db", "mykey", "myvalue",-1);
-			string value;
 			cache.Get<string>("db", "mykey", out value);
 			Assert.Equal("myvalue", value);
-			List<string> keys = new List<string>();
 			keys.Add("car");
 			keys.Add("brand");
-			List<string> values = new List<string>();
 			values.Add("1");
 			values.Add("toyota");
 			cache.SetAll<string>("DB", keys, values, 0);
