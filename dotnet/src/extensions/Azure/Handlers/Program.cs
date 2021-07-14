7025164c2171f762021a74bd87c9b5f620bb3433
sjuarezgx <sjuarez@genexus.com>
2021-07-14 09:59:31 -0300

    Fix to improve performance in azure functions execution (#387)
    *Added uni test cases.
    * Issue:88980

--- dotnet/src/extensions/Azure/Handlers/Program.cs
+++ dotnet/src/extensions/Azure/Handlers/Program.cs
@@ -2,18 +2,28 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Azure.Functions.Worker.Configuration;
+using System.Reflection;
+using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using GeneXus.Deploy.AzureFunctions.Handlers.Helpers;
 
-namespace AllFunctions
+namespace GeneXus.Deploy.AzureFunctions.Handlers
 {
     public class Program
     {
-        public static void Main()
+		static async Task Main()
         {
-            var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults()
-                .Build();
+			string roothPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-            host.Run();
+			var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+				.ConfigureServices(services =>
+				{
+					services.AddSingleton<ICallMappings, CallMappings>(x => new CallMappings(roothPath));
+				})
+				.Build();
+
+            await host.RunAsync();
         }
     }
 }
