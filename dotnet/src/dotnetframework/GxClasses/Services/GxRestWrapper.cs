82de939ee75c2e3b8a16a43c5acbcf8f21906ec7
Alejandro Panizza Carve <apanizza@genexus.com>
2021-10-06 15:52:57 -0300

    Update GxRestWrapper.cs
    (cherry picked from commit f27b740eb9d5d4a09bb50cc4297486a9cb156687)

--- dotnet/src/dotnetframework/GxClasses/Services/GxRestWrapper.cs
+++ dotnet/src/dotnetframework/GxClasses/Services/GxRestWrapper.cs
@@ -792,6 +792,7 @@ namespace GeneXus.Application
 				else
 				{
 					object o = MakeRestType(outputParameters[k]);
+
 					if (p !=null && p.SdtSerializeAsNull())
 					{						
 						outputParameters[k] = JNull.Value;
