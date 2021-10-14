dae3fa7386ba571354bf659894931c26f06b5e01
Gustavo Brown <gusbro@genexus.com>
2021-10-14 00:43:07 -0300

    add empty value for GX IsEmpty() method

--- dotnet/src/dotnetframework/DynService.Dynamo/DynamoDBDataStoreHelper.cs
+++ dotnet/src/dotnetframework/DynService.Dynamo/DynamoDBDataStoreHelper.cs
@@ -1,4 +1,5 @@
 using GeneXus.Data.Dynamo;
+using GeneXus.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,45 @@ namespace GeneXus.Data.NTier
 		public DynamoDBMap Map(string name)
 		{
 			return new DynamoDBMap(name);
+		}
+
+		public object empty(GXType gxtype)
+		{
+			switch(gxtype)
+			{
+				case GXType.Number:
+				case GXType.Int16:
+				case GXType.Int32:
+				case GXType.Int64: return 0;
+				case GXType.Date: 
+				case GXType.DateTime: 
+				case GXType.DateTime2:	return DateTimeUtil.NullDate();
+				case GXType.Byte:
+				case GXType.NChar:
+				case GXType.NClob:
+				case GXType.NVarChar:
+				case GXType.Char:
+				case GXType.LongVarChar:
+				case GXType.Clob:
+				case GXType.VarChar:
+				case GXType.Raw:
+				case GXType.Blob: return string.Empty;
+				case GXType.Boolean: return false;
+				case GXType.Decimal: return 0f;
+				case GXType.NText:
+				case GXType.Text:
+				case GXType.Image:
+				case GXType.UniqueIdentifier:
+				case GXType.Xml: return string.Empty;
+				case GXType.Geography: 
+				case GXType.Geopoint:
+				case GXType.Geoline:
+				case GXType.Geopolygon: return new Geospatial();
+				case GXType.DateAsChar: return string.Empty;
+				case GXType.Undefined:
+				default: return null;
+
+			}
 		}
 	}
 	
