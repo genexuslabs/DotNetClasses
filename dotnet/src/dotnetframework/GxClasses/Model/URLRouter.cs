99cdbbadaeee5e8f151e78d6c26537bf00fc57bd
Claudia Murialdo <cmurialdo@genexus.com>
2021-10-20 15:26:14 -0300

    Fix build error.

--- dotnet/src/dotnetframework/GxClasses/Model/URLRouter.cs
+++ dotnet/src/dotnetframework/GxClasses/Model/URLRouter.cs
@@ -15,6 +15,8 @@ namespace GeneXus.Application
 
 		static ConcurrentDictionary<string, string> routerList;
 		const string RESOURCE_PATTERN = "*.rewrite";
+		const string schemeRegEx = @"^([a-z][a-z0-9+\-.]*):";
+		static Regex scheme = new Regex(schemeRegEx, RegexOptions.IgnoreCase);
 		internal static string GetURLRoute(string key, object[] objectParms, string[] parmsName, string scriptPath)
 		{
 			string[] parms = objectParms.Select(p => StringizeParm(p)).ToArray();
