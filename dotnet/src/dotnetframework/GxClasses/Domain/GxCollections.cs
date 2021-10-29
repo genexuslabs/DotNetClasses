f18b81515d485c7750d3c9029b0739a6ff5f3552
Claudia Murialdo <cmurialdo@genexus.com>
2021-10-29 16:49:51 -0300

    Cherry pick branch 'genexuslabs:remove-var-warnings' into beta

--- dotnet/src/dotnetframework/GxClasses/Domain/GxCollections.cs
+++ dotnet/src/dotnetframework/GxClasses/Domain/GxCollections.cs
@@ -2241,7 +2241,7 @@ namespace GeneXus.Utils
 		public string ToJSonString()
 		{
 			JObject jObj = new JObject();
-			foreach (var item in this)
+			foreach (object item in this)
 			{
 
 				jObj.Accumulate(item.ToString(), this.Get(item.ToString()));
@@ -2295,7 +2295,7 @@ namespace GeneXus.Utils
 		public object GetJSONObject()
 		{
 			JObject jObj = new JObject();
-			foreach (var item in this)
+			foreach (object item in this)
 			{
 
 				jObj.Accumulate(item.ToString(), this.Get(item.ToString()));
@@ -2326,7 +2326,7 @@ namespace GeneXus.Utils
 		public override string ToString()
 		{
 			StringBuilder values = new StringBuilder();
-			foreach (var item in this)
+			foreach (object item in this)
 			{
 				values.Append(this[item.ToString()]);
 			}
@@ -2644,7 +2644,7 @@ namespace GeneXus.Utils
 			else if (ienumerableType != null)
 			{
 				IList lst = (IList)Activator.CreateInstance((typeof(List<>).MakeGenericType(ienumerableType)));
-				foreach (var item in i as IEnumerable)
+				foreach (object item in i as IEnumerable)
 					lst.Add(item);
 				o = lst;
 			}
