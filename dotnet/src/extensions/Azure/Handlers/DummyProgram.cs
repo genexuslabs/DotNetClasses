e6319957cb1b930d85794dfa630211a156eb1498
Claudia Murialdo <cmurialdo@genexus.com>
2021-11-11 13:48:42 -0300

    Unify project used for packaging Self-contained Azure Function binaries (which now is GeneXus.Deploy.AzureFunctions.Handlers.csproj) with the project used to build GeneXus.Deploy.AzureFunctions.Handlers.dll (which is GeneXus.Deploy.AzureFunctions.Handlers.csproj + References.targets). Package reference to Microsoft.Azure.Functions.Worker.Extensions.ApplicationInsights is twice (in GeneXus.Deploy.AzureFunctions.Handlers.csproj and References.targets) because the second one overrides the ExcludeAssets (to fix packaging error NU5104 with that preview package: A stable release of a package should not have a prerelease dependency)

---
+++ dotnet/src/extensions/Azure/Handlers/DummyProgram.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Azure.Functions.Worker.Configuration;
+
+namespace AllFunctions
+{
+    public class DummyProgram
+    {
+        public static void Main()
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .Build();
+            host.Run();
+        }
+    }
+}
