974241df4d9aff1850cd821e1e416246d856c176
Claudia Murialdo <cmurialdo@genexus.com>
2021-11-23 17:57:25 -0300

    Cherry pick branch 'genexuslabs:htmlclean-fixes' into beta

--- dotnet/src/dotnetframework/GxClasses/Properties/AssemblyInfo.cs
+++ dotnet/src/dotnetframework/GxClasses/Properties/AssemblyInfo.cs
@@ -5,6 +5,9 @@ using System.Resources;
 
 [assembly: AssemblyTitle("GeneXus Std Classes")]
 [assembly: AssemblyDescription("GeneXus Std Classes")]
-[assembly: AllowPartiallyTrustedCallers] 
-
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: InternalsVisibleTo("GxSearch,PublicKey=0024000004800000940000000602000000240000525341310004000001000100e52e348955bc6a"+
+"30ec9c7319f94e2ec18fd73d4445273f81bd2132e7309b52f3582b72d9dcd0231806e6eea947b4"+
+"0d948767cd8bdc33b738e2787bef3d0c8200979748684fe3aa4a39adc2e81e05750291dde1b823"+
+"62d82dc0e611c6048d51483f67bd656f21db0a70679b30ec744b3e66d518d25b66f66be72674f664c58c97")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
