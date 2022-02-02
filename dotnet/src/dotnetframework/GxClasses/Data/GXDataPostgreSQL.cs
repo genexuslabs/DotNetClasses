0521f95019f226c7aa589e1f1739605dc7eba42b
Claudia Murialdo <cmurialdo@genexus.com>
2022-02-02 10:35:43 -0300

    Add explicit prepare to npsql statements as suggested in documentation https://dev.to/vbilopav/postgresql-enhancements-in-norm-net-3g4b
    (cherry picked from commit f0b3f6a7ad5054565ddc703eefc6c20d7a309ba7)

--- dotnet/src/dotnetframework/GxClasses/Data/GXDataPostgreSQL.cs
+++ dotnet/src/dotnetframework/GxClasses/Data/GXDataPostgreSQL.cs
@@ -155,6 +155,10 @@ namespace GeneXus.Data
 			reader.Close();
 			return values;
 		}
+		protected override void PrepareCommand(IDbCommand cmd)
+		{
+			cmd.Prepare();
+		}
 		public override bool AllowsDuplicateParameters
 		{
 			get
