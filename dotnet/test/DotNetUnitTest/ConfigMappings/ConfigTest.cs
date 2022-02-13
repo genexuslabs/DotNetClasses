d4520a75eccb72a51432d76f42d740b589df4cf0
Claudia Murialdo <cmurialdo@genexus.com>
2022-02-13 14:40:58 -0300

    Cherry pick branch 'genexuslabs:ConfigEnvVar' into beta

---
+++ dotnet/test/DotNetUnitTest/ConfigMappings/ConfigTest.cs
@@ -0,0 +1,28 @@
+using GeneXus.Application;
+using Xunit;
+using System;
+using GeneXus.Configuration;
+using GeneXus.Data.NTier;
+
+namespace UnitTesting
+{
+	public class ConfigTest
+	{
+		[Fact]
+		public void ConMappingTest()
+		{
+			string customUser = "customUser";
+			string customDB = "customDB";
+			Environment.SetEnvironmentVariable("GXConnectionDefaultDB", customDB);
+			Environment.SetEnvironmentVariable("GXConnectionDefaultUser", customUser);
+			GxContext context = new GxContext();
+			DataStoreUtil.LoadDataStores(context);
+			string database, user;
+			Config.GetValueOf("Connection-Default-DB", out database);
+			Config.GetValueOf("Connection-Default-User", out user);
+			Assert.Contains(customDB, database, StringComparison.OrdinalIgnoreCase);
+			Assert.Contains(customUser, user, StringComparison.OrdinalIgnoreCase);
+		}
+
+	}
+}
