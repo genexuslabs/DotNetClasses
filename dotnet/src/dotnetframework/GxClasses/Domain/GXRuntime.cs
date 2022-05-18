6094f03a376ed6c3c93bb023510a1eabeb949de4
sjuarezgx <sjuarez@genexus.com>
2022-05-18 11:10:50 -0300

    Add methods for reading environment variables

--- dotnet/src/dotnetframework/GxClasses/Domain/GXRuntime.cs
+++ dotnet/src/dotnetframework/GxClasses/Domain/GXRuntime.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections;
+using GeneXus.Utils;
+
 namespace GX
 {
 	public class GXRuntime
@@ -10,6 +14,44 @@ namespace GX
 			}
 		}
 		public static int ExitCode { get; set; }
-
+		public static bool HasEnvironmentVariable(string varName)
+		{
+			if (string.IsNullOrEmpty(varName))
+				return false;
+			else
+			{
+				string value = System.Environment.GetEnvironmentVariable(varName);
+				if (value == null)
+					return false;
+				else
+					return true;
+			}
+		}
+		public static GXProperties GetEnvironmentVariables()
+		{
+			IDictionary variables = System.Environment.GetEnvironmentVariables();
+			GXProperties gXProperties = new GXProperties();
+			foreach (DictionaryEntry dentry in variables)
+			{
+				string key = (String)dentry.Key;
+				string value = (String)dentry.Value;
+				gXProperties.Add(key, value);
+				
+			}
+			return (gXProperties);
+		}
+		public static string GetEnvironmentVariable(string varName)
+		{
+			if (string.IsNullOrEmpty(varName))
+				return string.Empty;
+			else
+			{
+				string value = System.Environment.GetEnvironmentVariable(varName);
+				if (value == null)
+					return string.Empty;
+				else
+					return value;
+			}
+		}
 	}
 }
