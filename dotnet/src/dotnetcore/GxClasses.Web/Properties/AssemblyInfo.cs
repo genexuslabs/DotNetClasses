cd96d32b7835729acf34628c2617e6a7f2742cae
sjuarezgx <sjuarez@genexus.com>
2022-07-07 08:57:30 -0300

    Revert "Types that are not intended to be used outside of standard classes are changed from public to internal"
    This reverts commit 146f6e3567bd4beec195f834f77e839fa6dd786c.

--- dotnet/src/dotnetcore/GxClasses.Web/Properties/AssemblyInfo.cs
+++ dotnet/src/dotnetcore/GxClasses.Web/Properties/AssemblyInfo.cs
@@ -3,5 +3,3 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("DotNetCoreUnitTest")]
 [assembly: InternalsVisibleTo("GxNetCoreStartup")]
-[assembly: InternalsVisibleTo("GeneXus.Deploy.AzureFunctions.Handlers")]
-[assembly: InternalsVisibleTo("AzureFunctionsTest")]
--- dotnet/src/dotnetcore/GxClasses/Properties/AssemblyInfo.cs
+++ dotnet/src/dotnetcore/GxClasses/Properties/AssemblyInfo.cs
@@ -4,6 +4,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("GxClasses.Web")]
 [assembly: InternalsVisibleTo("GxSearch")]
 [assembly: InternalsVisibleTo("GxNetCoreStartup")]
-[assembly: InternalsVisibleTo("GeneXus.Deploy.AzureFunctions.Handlers")]
-[assembly: InternalsVisibleTo("AzureFunctionsTest")]
 
