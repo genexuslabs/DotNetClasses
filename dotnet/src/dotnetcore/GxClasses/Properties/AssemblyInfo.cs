f5da7c606738b79a902d4f289f32db03f1aa5c6c
aberta <aberta@genexus.com>
2022-07-07 11:14:02 -0300

    Restore "internal" definition for Types that are not intended to be used outside of standard classes are changed from public to internal""
    This partially reverts commit cd96d32b7835729acf34628c2617e6a7f2742cae.

--- dotnet/src/dotnetcore/GxClasses/Properties/AssemblyInfo.cs
+++ dotnet/src/dotnetcore/GxClasses/Properties/AssemblyInfo.cs
@@ -4,4 +4,6 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("GxClasses.Web")]
 [assembly: InternalsVisibleTo("GxSearch")]
 [assembly: InternalsVisibleTo("GxNetCoreStartup")]
+[assembly: InternalsVisibleTo("GeneXus.Deploy.AzureFunctions.Handlers")]
+[assembly: InternalsVisibleTo("AzureFunctionsTest")]
 
