dffd5f9872c4c7f75daa6e36a5127c2d0c39a16d
sgrampone <30872877+sgrampone@users.noreply.github.com>
2022-08-03 13:35:06 -0300

    Refactor GeneXusCryptography Module (#620)
    * Refactor GeneXusCryptography Module
    issue#96965
    
    * Hexadecimal output to upper case
    
    * Hash results to uppercase

--- dotnet/src/extensions/SecurityAPI/dotnet/dotnetframework/GeneXusCryptography/ChecksumUtils/CRCParameters.cs
+++ dotnet/src/extensions/SecurityAPI/dotnet/dotnetframework/GeneXusCryptography/ChecksumUtils/CRCParameters.cs
@@ -8,48 +8,48 @@ namespace GeneXusCryptography.ChecksumUtils
 	public struct CRCParameters
 #pragma warning restore CA1815 // Override equals and operator equals on value types
 	{
-        private int _width;  
-        public int Width
-        {
-            get => _width;
-        }
-        private long _polynomial; 
-        public long Polynomial
-        {
-            get => _polynomial;
-        }
-        private bool _reflectIn; 
-        public bool ReflectIn
-        {
-            get => _reflectIn;
-        }
-        private bool _reflectOut;   
-        public bool ReflectOut
-        {
-            get => _reflectOut;
-        }
-        private long _init; 
-        public long Init
-        {
-            get => _init;
-        }
-        private long _finalXor; 
-        public long FinalXor
-        {
-            get => _finalXor;
+		private int _width;
+		public int Width
+		{
+			get => _width;
+		}
+		private long _polynomial;
+		public long Polynomial
+		{
+			get => _polynomial;
+		}
+		private bool _reflectIn;
+		public bool ReflectIn
+		{
+			get => _reflectIn;
+		}
+		private bool _reflectOut;
+		public bool ReflectOut
+		{
+			get => _reflectOut;
+		}
+		private long _init;
+		public long Init
+		{
+			get => _init;
+		}
+		private long _finalXor;
+		public long FinalXor
+		{
+			get => _finalXor;
 
-        }
+		}
 
-        [SecuritySafeCritical]
-        public CRCParameters(int width, long polynomial, long init, bool reflectIn, bool reflectOut, long finalXor)
-        {
-            _width = width;
-            _polynomial = polynomial;
-            _reflectIn = reflectIn;
-            _reflectOut = reflectOut;
-            _init = init;
-            _finalXor = finalXor;
-        }
+		[SecuritySafeCritical]
+		public CRCParameters(int width, long polynomial, long init, bool reflectIn, bool reflectOut, long finalXor)
+		{
+			_width = width;
+			_polynomial = polynomial;
+			_reflectIn = reflectIn;
+			_reflectOut = reflectOut;
+			_init = init;
+			_finalXor = finalXor;
+		}
 
-    }
+	}
 }
