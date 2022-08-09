8625e847f59e56d148bc3c311494bc38db6d9cbc
Gastón Milano <gmilano@genexus.onmicrosoft.com>
2022-08-09 13:43:52 -0300

    Cherry pick branch 'genexuslabs:veracode1003' into beta

--- dotnet/test/DotNetCoreUnitTest/StringUtil/StringUtilTests.cs
+++ dotnet/test/DotNetCoreUnitTest/StringUtil/StringUtilTests.cs
@@ -32,9 +32,7 @@ namespace xUnitTesting
 			json +="</kml>";
 
 			string expectedJsonEncoded = "<kml><Document>\\t<Style id=\\\"MyLine\\\">\\t\\t<LineStyle>\\t\\t\\t<color>802080ff</color>\\t\\t\\t<width>6</width>\\t\\t</LineStyle>\\t</Style>\\t<Placemark>\\t\\t<LineString>\\t\\t\\t<coordinates>-88.076680,43.945580 -88.077480,43.945930 -88.078530,43.946390 -88.078960</coordinates>\\t\\t</LineString>\\t\\t<styleUrl>#MyLine</styleUrl>\\t</Placemark></Document></kml>";
-
 			string jsonEncoded = StringUtil.JSONEncode(json);
-			File.WriteAllText(@"C:\temp\json.txt", jsonEncoded);
 			Assert.Equal(jsonEncoded, expectedJsonEncoded);
 
 		}
