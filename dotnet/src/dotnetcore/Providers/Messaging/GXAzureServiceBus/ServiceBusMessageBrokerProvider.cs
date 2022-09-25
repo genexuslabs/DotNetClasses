7df21094093fc344474cd623e547bc6685abf87d
sjuarezgx <sjuarez@genexus.com>
2022-09-25 19:15:56 -0300

    remove debugger lines

--- dotnet/src/dotnetcore/Providers/Messaging/GXAzureServiceBus/ServiceBusMessageBrokerProvider.cs
+++ dotnet/src/dotnetcore/Providers/Messaging/GXAzureServiceBus/ServiceBusMessageBrokerProvider.cs
@@ -9,7 +9,6 @@ namespace GeneXus.Messaging.GXAzureServiceBus
 	{
 		public MessageQueue Connect(string queueName, string connectionString, out GXBaseCollection<SdtMessages_Message> errorMessages, out bool success)
 		{
-			System.Diagnostics.Debugger.Launch();
 			MessageBrokerProvider messageBrokerProvider = new MessageBrokerProvider();
 			GXProperties properties = new GXProperties();
 			properties.Add(PropertyConstants.MESSAGEBROKER_AZURESB_QUEUENAME, queueName);
@@ -23,7 +22,6 @@ namespace GeneXus.Messaging.GXAzureServiceBus
 
 		public MessageQueue Connect(string topicName, string subcriptionName, string connectionString, out GXBaseCollection<SdtMessages_Message> errorMessages, out bool success)
 		{
-			System.Diagnostics.Debugger.Launch();
 			MessageBrokerProvider messageBrokerProvider = new MessageBrokerProvider();
 			GXProperties properties = new GXProperties();
 			properties.Add(PropertyConstants.MESSAGEBROKER_AZURESB_TOPICNAME, topicName);
@@ -37,7 +35,6 @@ namespace GeneXus.Messaging.GXAzureServiceBus
 		}
 		public MessageQueue Connect(string queueName, string connectionString, bool sessionEnabled, GxUserType receiverOptions, string senderIdentifier, out GXBaseCollection<SdtMessages_Message> errorMessages, out bool success)
 		{
-			System.Diagnostics.Debugger.Launch();
 			MessageBrokerProvider messageBrokerProvider = new MessageBrokerProvider();
 
 			ReceiverOptions options = TransformGXUserTypeToReceiverOptions(receiverOptions);
@@ -60,7 +57,6 @@ namespace GeneXus.Messaging.GXAzureServiceBus
 
 		public MessageQueue Connect(string topicName, string subcriptionName, string connectionString, bool sessionEnabled, GxUserType receiverOptions, string senderIdentifier, out GXBaseCollection<SdtMessages_Message> errorMessages, out bool success)
 		{
-			System.Diagnostics.Debugger.Launch();
 			MessageBrokerProvider messageBrokerProvider = new MessageBrokerProvider();
 			GXProperties properties = new GXProperties();
 			ReceiverOptions options = TransformGXUserTypeToReceiverOptions(receiverOptions);
