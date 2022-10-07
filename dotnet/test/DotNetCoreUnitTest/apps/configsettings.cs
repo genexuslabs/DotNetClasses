272efe83dfc37e44f8eb2039ac3391e2ca1e0e95
cmurialdo <cmurialdo@genexus.com>
2022-10-07 07:12:57 -0300

    Remove unused usings.

--- dotnet/test/DotNetCoreUnitTest/apps/configsettings.cs
+++ dotnet/test/DotNetCoreUnitTest/apps/configsettings.cs
@@ -1,26 +1,8 @@
-using System;
-using System.Collections;
-using GeneXus.Utils;
-using GeneXus.Resources;
 using GeneXus.Application;
-using GeneXus.Metadata;
-using GeneXus.Cryptography;
-using com.genexus;
-using GeneXus.Data.ADO;
-using GeneXus.Data.NTier;
-using GeneXus.Data.NTier.ADO;
-using GeneXus.WebControls;
-using GeneXus.Http;
-using GeneXus.Procedure;
-using GeneXus.XML;
-using GeneXus.Search;
-using GeneXus.Encryption;
-using GeneXus.Http.Client;
-using GeneXus.Http.Server;
-using System.Threading;
-using System.Xml.Serialization;
-using System.Runtime.Serialization;
 using GeneXus.Configuration;
+using GeneXus.Data.NTier;
+using GeneXus.Http.Server;
+using GeneXus.Procedure;
 
 namespace GeneXus.Programs.apps
 {
--- dotnet/test/DotNetCoreUnitTest/apps/httpcors.cs
+++ dotnet/test/DotNetCoreUnitTest/apps/httpcors.cs
@@ -1,25 +1,7 @@
-using System;
-using System.Collections;
-using GeneXus.Utils;
-using GeneXus.Resources;
 using GeneXus.Application;
-using GeneXus.Metadata;
-using GeneXus.Cryptography;
-using com.genexus;
-using GeneXus.Data.ADO;
 using GeneXus.Data.NTier;
-using GeneXus.Data.NTier.ADO;
-using GeneXus.WebControls;
-using GeneXus.Http;
-using GeneXus.Procedure;
-using GeneXus.XML;
-using GeneXus.Search;
-using GeneXus.Encryption;
-using GeneXus.Http.Client;
 using GeneXus.Http.Server;
-using System.Threading;
-using System.Xml.Serialization;
-using System.Runtime.Serialization;
+using GeneXus.Procedure;
 
 namespace GeneXus.Programs.apps
 {
