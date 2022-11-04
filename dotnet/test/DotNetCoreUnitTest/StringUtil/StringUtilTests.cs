3b2b3e3617c64b4b57ad6b65df8ecf27b17e4dc2
cmurialdo <cmurialdo@genexus.com>
2022-11-04 19:00:03 -0300

    Fix on FromJSONObject method of SDTs which was deserializing empty datetimes in the wrong way.

--- dotnet/test/DotNetCoreUnitTest/StringUtil/StringUtilTests.cs
+++ dotnet/test/DotNetCoreUnitTest/StringUtil/StringUtilTests.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using GeneXus.Programs;
 using GeneXus.Utils;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace xUnitTesting
@@ -36,5 +32,19 @@ namespace xUnitTesting
 			Assert.Equal(jsonEncoded, expectedJsonEncoded);
 
 		}
+		[Fact]
+		public void TestFromJsonSDTWithBlankDateTime()
+		{
+			SdtSDT1_SDT1Item sdt = new SdtSDT1_SDT1Item();
+			Jayrock.Json.JObject json = new Jayrock.Json.JObject();
+			json["SDT1_DateTime"] = "           00:00:00";
+			json["SDT1_Name"]=string.Empty;
+			json["SDT1_No"] = 0;
+
+			sdt.FromJSONObject(json);
+			Assert.Equal(sdt.gxTpr_Sdt1_datetime, DateTimeUtil.NullDate());
+		}
+
+		
 	}
 }
