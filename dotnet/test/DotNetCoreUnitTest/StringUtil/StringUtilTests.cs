253aebc2da01bdbb43f0bc5d64bafc9a26ffeeac
cmurialdo <cmurialdo@genexus.com>
2022-11-09 15:50:55 -0300

    Cherry pick branch 'genexuslabs:fromjson-blank-empty-datetime' into beta

--- dotnet/test/DotNetCoreUnitTest/StringUtil/StringUtilTests.cs
+++ dotnet/test/DotNetCoreUnitTest/StringUtil/StringUtilTests.cs
@@ -45,6 +45,20 @@ namespace xUnitTesting
 			Assert.Equal(sdt.gxTpr_Sdt1_datetime, DateTimeUtil.NullDate());
 		}
 
+		[Fact]
+		public void TestFromJsonSDTWithTimeMustNotApplyTimezone()
+		{
+			SdtSDT1_SDT1Item sdt = new SdtSDT1_SDT1Item();
+			Jayrock.Json.JObject json = new Jayrock.Json.JObject();
+			json["SDT1_DateTime"] = "2014-04-29T14:29:40";
+			json["SDT1_Name"] = string.Empty;
+			json["SDT1_No"] = 0;
+
+			sdt.FromJSONObject(json);
+			Assert.Equal(14, sdt.gxTpr_Sdt1_datetime.Hour);
+			Assert.Equal(29, sdt.gxTpr_Sdt1_datetime.Minute);
+		}
+
 		
 	}
 }
