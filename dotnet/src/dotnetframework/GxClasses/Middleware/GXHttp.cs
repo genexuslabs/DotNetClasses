1a75462cbb3e56b253930c0a72d22ce8b421ee13
ARTECH\sgrampone <sgrampone@genexus.com>
2022-12-08 18:21:06 -0300

    Log sanitization
    (cherry picked from commit 726c33fa6b515e0f73f37f0f9dc50aebf91fdab7)

--- dotnet/src/dotnetframework/GxClasses/Middleware/GXHttp.cs
+++ dotnet/src/dotnetframework/GxClasses/Middleware/GXHttp.cs
@@ -2122,7 +2122,7 @@ namespace GeneXus.Http
 		private void SendHeaders()
 		{
 			sendCacheHeaders();
-			GXLogging.Debug(log, "HttpHeaders: ", DumpHeaders(localHttpContext));
+			GXLogging.DebugSanitized(log, "HttpHeaders: ", DumpHeaders(localHttpContext));
 			sendAdditionalHeaders();
 			HttpHelper.CorsHeaders(localHttpContext);
 			HttpHelper.AllowHeader(localHttpContext, new List<string>() { $"{HttpMethod.Get.Method},{HttpMethod.Post.Method}" });
