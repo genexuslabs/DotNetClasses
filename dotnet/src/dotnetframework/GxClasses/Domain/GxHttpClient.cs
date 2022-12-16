3451ba97b3f06f5d0d1a221aa6e6f753ef816826
sgrampone <30872877+sgrampone@users.noreply.github.com>
2022-12-16 11:38:19 -0300

    Fortify - Log Forging mitigation (#734)
    * Adds a sanitization function using a white list as indicated by Fortify SAST. The function replaces all special characters other than _- and : with a blanc space to avoid any king of log injection.
    
    * Change sanitization function.
    
    * Change log sanitization function
    
    * Change log sanitization function
    
    * Change log sanitization function
    
    * Log sanitization
    
    * Log sanitization
    
    * Log sanitization
    
    * Log sanitization on jwt
    
    * Load whitelist for sanitization just once
    
    * Fix build issue on nwt framework
    
    * Fix log sanitization

--- dotnet/src/dotnetframework/GxClasses/Domain/GxHttpClient.cs
+++ dotnet/src/dotnetframework/GxClasses/Domain/GxHttpClient.cs
@@ -842,7 +842,7 @@ namespace GeneXus.Http.Client
 				_errDescription = "The remote server returned an error: (" + _statusCode + ") " + _statusDescription + ".";
 			}
 			ClearSendStream();
-			GXLogging.Debug(log, "_responseString " + ToString());
+			GXLogging.DebugSanitized(log, "_responseString " + ToString());
 		}
 		NameValueCollection _respHeaders;
 		private bool disposedValue;
@@ -1239,7 +1239,7 @@ namespace GeneXus.Http.Client
 			_statusDescription = resp.StatusDescription;
 			resp.Close();
 			ClearSendStream();
-			GXLogging.Debug(log, "_responseString " + ToString());
+			GXLogging.DebugSanitized(log, "_responseString " + ToString());
 
 		}
 		private Encoding GetEncoding(string charset)
