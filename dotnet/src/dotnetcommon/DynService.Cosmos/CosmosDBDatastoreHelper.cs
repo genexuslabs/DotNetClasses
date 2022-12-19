bba9e671432e40631214922fadc4b16550d5acc1
sjuarezgx <sjuarez@genexus.com>
2022-12-19 12:47:34 -0300

    add empty method used by automatic prompts

--- dotnet/src/dotnetcommon/DynService.Cosmos/CosmosDBDatastoreHelper.cs
+++ dotnet/src/dotnetcommon/DynService.Cosmos/CosmosDBDatastoreHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GeneXus.Data.Cosmos;
+using GeneXus.Utils;
 
 namespace GeneXus.Data.NTier
 {
@@ -19,7 +20,44 @@ namespace GeneXus.Data.NTier
 		{
 			return new CosmosDBMap(name);
 		}
+		public object empty(GXType gxtype)
+		{
+			switch (gxtype)
+			{
+				case GXType.Number:
+				case GXType.Int16:
+				case GXType.Int32:
+				case GXType.Int64: return 0;
+				case GXType.Date:
+				case GXType.DateTime:
+				case GXType.DateTime2: return DateTimeUtil.NullDate();
+				case GXType.Byte:
+				case GXType.NChar:
+				case GXType.NClob:
+				case GXType.NVarChar:
+				case GXType.Char:
+				case GXType.LongVarChar:
+				case GXType.Clob:
+				case GXType.VarChar:
+				case GXType.Raw:
+				case GXType.Blob: return string.Empty;
+				case GXType.Boolean: return false;
+				case GXType.Decimal: return 0f;
+				case GXType.NText:
+				case GXType.Text:
+				case GXType.Image:
+				case GXType.UniqueIdentifier:
+				case GXType.Xml: return string.Empty;
+				case GXType.Geography:
+				case GXType.Geopoint:
+				case GXType.Geoline:
+				case GXType.Geopolygon: return new Geospatial();
+				case GXType.DateAsChar: return string.Empty;
+				case GXType.Undefined:
+				default: return null;
 
+			}
+		}
 	}
 	public class CosmosDBQuery : Query
 	{
