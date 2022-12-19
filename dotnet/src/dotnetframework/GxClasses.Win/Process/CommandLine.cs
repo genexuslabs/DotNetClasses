29d0fb5efbcddf10c62015e087a5d566a13e0e52
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2022-12-19 17:38:49 -0300

    Working directory of fully qualified shell files was changed by mistake at https://github.com/genexuslabs/DotNetClasses/pull/709 (#742)
    (cherry picked from commit 62cd7ff1713e41f1b8d8176a9e9e0c1661d30c26)

--- dotnet/src/dotnetframework/GxClasses.Win/Process/CommandLine.cs
+++ dotnet/src/dotnetframework/GxClasses.Win/Process/CommandLine.cs
@@ -129,11 +129,7 @@ namespace GeneXus.Utils
 					//If WorkingDirectory is an empty string, the current directory is understood to contain the executable.
 					try
 					{
-						if (Path.IsPathRooted(file))
-						{
-							p.StartInfo.WorkingDirectory = Path.GetDirectoryName(file);
-						}
-						else
+						if (!Path.IsPathRooted(file))
 						{
 							p.StartInfo.WorkingDirectory = GxContext.StaticPhysicalPath();
 						}
