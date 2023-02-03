e442f6eadb0484efebd262fc6d08a336b01f2208
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2023-02-03 17:32:41 -0300

    Remove deprecated DynService.Fabric package. (#764)

--- dotnet/src/dotnetframework/DynServiceFabric/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("DynService.Fabric")]
-
-[assembly: ComVisible(false)]
-
