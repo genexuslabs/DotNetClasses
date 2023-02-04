e2a4ed3ef4e7fc081406fbd4f28036bc0c562723
cmurialdo <cmurialdo@genexus.com>
2023-02-04 15:05:10 -0300

    Setup ContentRoot also so appsettings.json is found.

--- dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
+++ dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
@@ -76,6 +76,7 @@ namespace GeneXus.Application
 		   WebHost.CreateDefaultBuilder(args)
 			.ConfigureLogging(logging => logging.AddConsole())
 			.UseStartup<Startup>()
+			.UseContentRoot(Startup.LocalPath)
 			.Build();
 
 		public static IWebHost BuildWebHostPort(string[] args, string port)
@@ -88,6 +89,7 @@ namespace GeneXus.Application
 				 .ConfigureLogging(logging => logging.AddConsole())
 				 .UseUrls($"{schema}://*:{port}")
 				.UseStartup<Startup>()
+				.UseContentRoot(Startup.LocalPath)
 				.Build();
 		}
 		private static void LocatePhysicalLocalPath()
