c92d940164e51f9276e7200d618927f91cb78b1a
sjuarezgx <sjuarez@genexus.com>
2023-05-09 12:17:01 -0300

    Cherry pick branch 'genexuslabs:Otel_AzureMonitor' into beta

--- dotnet/src/dotnetcore/Providers/OpenTelemetry/OpenTelemetryAzureMonitor/AzureAppInsights.cs
+++ dotnet/src/dotnetcore/Providers/OpenTelemetry/OpenTelemetryAzureMonitor/AzureAppInsights.cs
@@ -6,35 +6,48 @@ using OpenTelemetry.Trace;
 using GeneXus.Services;
 using System.Diagnostics;
 using Azure.Monitor.OpenTelemetry.Exporter;
+using log4net;
 
 namespace GeneXus.OpenTelemetry.Azure
 {
 	public class AzureAppInsights : IOpenTelemetryProvider
 	{
+		private static readonly ILog log = LogManager.GetLogger(typeof(AzureAppInsights));
+		private const string AZURE_OTEL_EXPORTER_CONNECTIONSTRING = "AZURE_OTEL_EXPORTER_CONNECTIONSTRING";
+		private const string ACTIVITY_SOURCE = "OTel.AzureMonitor.GXApp";
+		private const string ACTIVITY_NAME = "GeneXusApplicationActivity";
+
 		public AzureAppInsights(GXService s)
 		{
 		}
 
-		private static readonly ActivitySource GXActivitySource = new ActivitySource(
-		"OTel.AzureMonitor.GXApp");
+		private static readonly ActivitySource GXActivitySource = new ActivitySource(ACTIVITY_SOURCE);
 
 		public bool InstrumentAspNetCoreApplication(IServiceCollection _)
 		{
-			string oltpEndpoint = Environment.GetEnvironmentVariable("AZURE_OTEL_EXPORTER_CONNECTIONSTRING");
+			string oltpEndpoint = Environment.GetEnvironmentVariable(AZURE_OTEL_EXPORTER_CONNECTIONSTRING);
+		
+			if (!string.IsNullOrEmpty(oltpEndpoint))
+			{ 
+				using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+					.AddAzureMonitorTraceExporter(o =>
+					{
+						o.ConnectionString = oltpEndpoint;
+					})
+					.AddSource(ACTIVITY_SOURCE)
+					.AddGxAspNetInstrumentation()
+					.Build();
 
-			using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-				.AddAzureMonitorTraceExporter(o =>
+				using (var activity = GXActivitySource.StartActivity(ACTIVITY_NAME))
 				{
-					o.ConnectionString = oltpEndpoint;
-				})
-				.AddGxAspNetInstrumentation()
-				.Build();
-
-			using (var activity = GXActivitySource.StartActivity("GXAppActivity", ActivityKind.Server))
-			{
+				}
+				return true;
 			}
-
-			return true;
+			else
+			{ 
+				log.Warn("OpenTelemetry Azure Monitor was not initialized due to missing 'AZURE_OTEL_EXPORTER_CONNECTIONSTRING' Environment Variable");
+				return false;
+			}
 		}
 	}
 }
