375e15f3a03168fa6553dea0ca7674bd0c526efd
sjuarezgx <sjuarez@genexus.com>
2023-06-17 19:03:54 -0300

    Cherry pick branch 'genexuslabs:AzureFunc_Blob_Trigger' into beta

--- dotnet/src/extensions/Azure/Handlers/Helpers/EventSourceType.cs
+++ dotnet/src/extensions/Azure/Handlers/Helpers/EventSourceType.cs
@@ -6,5 +6,6 @@ namespace GeneXus.Deploy.AzureFunctions.Handlers.Helpers
 		public const string ServiceBusMessage = "ServiceBusMessage";
 		public const string Timer = "Timer";
 		public const string CosmosDB = "CosmosDB";
+		public const string Blob = "Blob";
 	}
 }
