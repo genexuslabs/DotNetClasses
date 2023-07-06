6f619e8c4b9a5bc6f951e6daef60b0cb6345a0c1
cmurialdo <cmurialdo@genexus.com>
2023-07-06 14:49:51 -0300

    Specify event name on Parameter Mismatch Error Message

--- dotnet/src/dotnetframework/GxClasses/Core/GXApplication.cs
+++ dotnet/src/dotnetframework/GxClasses/Core/GXApplication.cs
@@ -3173,7 +3173,7 @@ namespace GeneXus.Application
 			{
 				inBeforeCommit = true;
 				if (beforeCommitObj != null)
-					ClassLoader.ExecuteVoidRef(beforeCommitObj, "execute", new Object[] { callerName });
+					ClassLoader.ExecuteVoidRef(beforeCommitObj, "execute", new Object[] { callerName }, "Before Commit");
 				inBeforeCommit = false;
 			}
 		}
@@ -3184,7 +3184,7 @@ namespace GeneXus.Application
 			{
 				inAfterCommit = true;
 				if (afterCommitObj != null)
-					ClassLoader.ExecuteVoidRef(afterCommitObj, "execute", new Object[] { callerName });
+					ClassLoader.ExecuteVoidRef(afterCommitObj, "execute", new Object[] { callerName },"After Commit");
 				inAfterCommit = false;
 			}
 		}
@@ -3195,7 +3195,7 @@ namespace GeneXus.Application
 			{
 				inBeforeRollback = true;
 				if (beforeRollbackObj != null)
-					ClassLoader.ExecuteVoidRef(beforeRollbackObj, "execute", new Object[] { callerName });
+					ClassLoader.ExecuteVoidRef(beforeRollbackObj, "execute", new Object[] { callerName }, "Before Rollback");
 				inBeforeRollback = false;
 			}
 		}
@@ -3206,7 +3206,7 @@ namespace GeneXus.Application
 			{
 				inAfterRollback = true;
 				if (afterRollbackObj != null)
-					ClassLoader.ExecuteVoidRef(afterRollbackObj, "execute", new Object[] { callerName });
+					ClassLoader.ExecuteVoidRef(afterRollbackObj, "execute", new Object[] { callerName }, "After Rollback");
 				inAfterRollback = false;
 			}
 		}
@@ -3216,7 +3216,7 @@ namespace GeneXus.Application
 			if (beforeConnectObj != null)
 			{
 				GXLogging.Debug(log, "ExecuteBeforeConnect");
-				ClassLoader.ExecuteVoidRef(beforeConnectObj, "execute", new Object[] { datastore });
+				ClassLoader.ExecuteVoidRef(beforeConnectObj, "execute", new Object[] { datastore }, "Before Connect");
 				return true;
 			}
 			else
@@ -3230,7 +3230,7 @@ namespace GeneXus.Application
 			if (afterConnectObj != null)
 			{
 				GXLogging.Debug(log, "ExecuteAfterConnect");
-				ClassLoader.ExecuteVoidRef(afterConnectObj, "execute", new Object[] { datastoreName });
+				ClassLoader.ExecuteVoidRef(afterConnectObj, "execute", new Object[] { datastoreName }, "After Connect");
 				return true;
 			}
 			else
