3b8aa350c1280f8d00c979f2c353c8fafb30256f
sjuarezgx <sjuarez@genexus.com>
2023-07-18 19:44:50 -0300

    Azure Event Grid API implementation

---
+++ dotnet/src/dotnetcore/Providers/Messaging/GXAzureEventGrid/EventGridRouterProvider.cs
@@ -0,0 +1,23 @@
+using GeneXus.Messaging.Common;
+using GeneXus.Utils;
+
+namespace GeneXus.Messaging.GXAzureEventGrid
+{
+	public class EventGridRouterProvider
+	{
+		public EventRouterProviderBase Connect(string endpoint, string accesskey, out GXBaseCollection<SdtMessages_Message> errorMessages, out bool success)
+		{
+			EventRouterProvider eventRouterProvider = new EventRouterProvider();
+			GXProperties properties = new GXProperties
+			{
+				{ PropertyConstants.EVENTROUTER_AZUREEG_ENDPOINT, endpoint },
+				{ PropertyConstants.EVENTROUTER_AZUREEG_ACCESS_KEY, accesskey }
+			};
+
+			EventRouterProviderBase evtRouterProvider = eventRouterProvider.Connect(PropertyConstants.AZUREEVENTGRID, properties, out GXBaseCollection<SdtMessages_Message> errorMessagesConnect, out bool successConnect);
+			errorMessages = errorMessagesConnect;
+			success = successConnect;
+			return evtRouterProvider;
+		}
+	}
+}
