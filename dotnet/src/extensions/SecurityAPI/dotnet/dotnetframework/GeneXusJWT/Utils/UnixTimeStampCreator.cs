e318bdab4d0c1a64d816bbfedb87735164832eae
ARTECH\sgrampone <sgrampone@genexus.com>
2023-07-20 14:29:30 -0300

    UnixTimestamp implementation

---
+++ dotnet/src/extensions/SecurityAPI/dotnet/dotnetframework/GeneXusJWT/Utils/UnixTimeStampCreator.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Globalization;
+using System.Security;
+using SecurityAPICommons.Commons;
+
+namespace GeneXusJWT.Utils
+{
+	[SecuritySafeCritical]
+	public class UnixTimeStampCreator: SecurityAPIObject
+	{
+		[SecuritySafeCritical]
+		public UnixTimeStampCreator() : base() { }
+
+		[SecuritySafeCritical]
+		public string Create(string date)
+		{
+			long newdate;
+			try
+			{
+				newdate= (long)DateTime.ParseExact(date, "yyyy/MM/dd HH:mm:ss", CultureInfo.InvariantCulture).ToUniversalTime().Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+			}
+			catch (Exception)
+			{
+				error.setError("UTS01", "Date format error; expected yyyy/MM/dd HH:mm:ss");
+				return "";
+			}
+			return newdate.ToString(CultureInfo.InvariantCulture);
+		}
+	}
+}
