353fdf3d5cedb313fad7fe44ea4b837fd57c233f
sjuarezgx <sjuarez@genexus.com>
2023-08-16 12:36:02 -0300

    Cherry pick branch 'genexuslabs:Issue104244' into beta

--- dotnet/src/dotnetcore/Providers/Messaging/GXMessageBroker/MessageBrokerProvider.cs
+++ dotnet/src/dotnetcore/Providers/Messaging/GXMessageBroker/MessageBrokerProvider.cs
@@ -75,8 +75,7 @@ namespace GeneXus.Messaging.Common
 					SetEncryptedProperty(properties, PropertyConstants.MESSAGEBROKER_AZURESB_QUEUENAME);
 					SetEncryptedProperty(properties, PropertyConstants.MESSAGEBROKER_AZURESB_SUBSCRIPTION_NAME);
 					SetEncryptedProperty(properties, PropertyConstants.MESSAGEBROKER_AZURESB_CONNECTIONSTRING);
-					SetEncryptedProperty(properties, PropertyConstants.MESSAGEBROKER_AZURESB_FULLYQUALIFIEDNAMESPACE);
-					SetEncryptedProperty(properties, PropertyConstants.AUTHENTICATION_METHOD);
+					SetEncryptedProperty(properties, PropertyConstants.MESSAGEBROKER_AZURESB_FULLYQUALIFIEDNAMESPACE);				
 					if (string.IsNullOrEmpty(providerService.ClassName) || !providerService.ClassName.Contains(className))
 					{
 						providerService.ClassName = PropertyConstants.AZURE_SB_PROVIDER_CLASSNAME;
