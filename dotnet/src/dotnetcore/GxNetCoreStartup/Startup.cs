0911b37f27e31ff4ff62fbe720c907ba80cf59c7
Claudia Murialdo <cmurialdo@genexus.com>
2023-08-30 06:58:12 -0300

    Cherry pick branch 'genexuslabs:csrftoken-cookie-domain' into beta

--- dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
+++ dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
@@ -684,8 +684,13 @@ namespace GeneXus.Application
 			{
 				cookieOptions.SameSite = sameSiteMode;
 			}
+			Uri referer = context.Request.GetUrlReferrer();
+			if (referer != null)
+			{
+				cookieOptions.Domain = referer.Host;
+			}
 			context.Response.Cookies.Append(HttpHeader.X_GXCSRF_TOKEN, tokenSet.RequestToken, cookieOptions);
-			GXLogging.Debug(log, $"Setting cookie ", HttpHeader.X_GXCSRF_TOKEN, "=", tokenSet.RequestToken, " samesite:" + sameSiteMode);
+			GXLogging.Debug(log, $"Setting cookie ", HttpHeader.X_GXCSRF_TOKEN, "=", tokenSet.RequestToken, " samesite:" + sameSiteMode, " domain:" + cookieOptions.Domain);
 		}
 
 	}
