ac5bbcad203730479a0da4c329d56827192707ed
sjuarezgx <sjuarez@genexus.com>
2023-09-25 16:30:51 -0300

    fix merge error

--- dotnet/src/dotnetcore/Providers/OpenTelemetry/OpenTelemetryAzureMonitor/AzureAppInsights.cs
+++ dotnet/src/dotnetcore/Providers/OpenTelemetry/OpenTelemetryAzureMonitor/AzureAppInsights.cs
@@ -1,15 +1,9 @@
 using System;
-using Azure.Identity;
-using Azure.Monitor.OpenTelemetry.Exporter;
+using Azure.Monitor.OpenTelemetry.AspNetCore;
 using GeneXus.Services;
 using GeneXus.Services.OpenTelemetry;
-using log4net;
 using Microsoft.Extensions.DependencyInjection;
-using OpenTelemetry;
-using OpenTelemetry.Logs;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
+using Microsoft.Extensions.Logging;
 
 namespace GeneXus.OpenTelemetry.Azure
 {
@@ -22,45 +16,20 @@ namespace GeneXus.OpenTelemetry.Azure
 		{
 		}
 
-		public bool InstrumentAspNetCoreApplication(IServiceCollection _)
+		public bool InstrumentAspNetCoreApplication(IServiceCollection services)
 		{
 			string oltpEndpoint = Environment.GetEnvironmentVariable(APPLICATIONINSIGHTS_CONNECTION_STRING);
-			try
+		
+			if (!string.IsNullOrEmpty(oltpEndpoint))
 			{
-				var resourceBuilder = ResourceBuilder.CreateDefault()
-					.AddTelemetrySdk();
-
-				Sdk.CreateTracerProviderBuilder()
-				.SetResourceBuilder(resourceBuilder)
-				.AddAzureMonitorTraceExporter(o =>
-				{
-					if (!string.IsNullOrEmpty(oltpEndpoint))
-						o.ConnectionString = oltpEndpoint;
-					else
+				services.AddOpenTelemetry()
+				.UseAzureMonitor( o =>
 					{
-						o.Credential = new DefaultAzureCredential();
-						log.Debug("Connect to Azure monitor Opentelemetry Trace exporter using Default Azure credential");
-					}
-				})
-				.AddGxAspNetInstrumentation()
-				.Build();
-
-				Sdk.CreateMeterProviderBuilder()
-				.SetResourceBuilder(resourceBuilder)
-				.AddAzureMonitorMetricExporter(o =>
-				{
-					if (!string.IsNullOrEmpty(oltpEndpoint))
 						o.ConnectionString = oltpEndpoint;
-					else
-					{
-						o.Credential = new DefaultAzureCredential();
-						log.Debug("Connect to Azure monitor Opentelemetry Metrics exporter using Default Azure credential");
-					}
-				})
-				.Build();
+					});
+
 				return true;
 			}
-
 			else
 			{
 				GXLogging.Warn(log, "OpenTelemetry Azure Monitor was not initialized due to missing 'APPLICATIONINSIGHTS_CONNECTION_STRING' Environment Variable");
