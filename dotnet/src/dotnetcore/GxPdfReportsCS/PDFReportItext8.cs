d08f3aa05c17a5118f47ae532510c4abc711f48a
Claudia Murialdo <cmurialdo@genexus.com>
2023-09-27 09:59:43 -0300

    Remove unused using.

--- dotnet/src/dotnetcore/GxPdfReportsCS/PDFReportItext8.cs
+++ dotnet/src/dotnetcore/GxPdfReportsCS/PDFReportItext8.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Text;
 using GeneXus;
 using iText.Barcodes;
