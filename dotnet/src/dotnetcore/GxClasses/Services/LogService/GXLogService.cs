16f93e83d5a745468563089f2aaad1d38fe2e123
sjuarezgx <sjuarez@genexus.com>
2023-10-22 14:56:07 -0300

    Cherry pick branch 'genexuslabs:LoggingAPIImpl_Update' into beta

--- dotnet/src/dotnetcore/GxClasses/Services/LogService/GXLogService.cs
+++ dotnet/src/dotnetcore/GxClasses/Services/LogService/GXLogService.cs
@@ -1,5 +1,6 @@
 using System;
 using GeneXus.Configuration;
+using GeneXus.Services.OpenTelemetry;
 using Microsoft.Extensions.Logging;
 
 namespace GeneXus.Services.Log
@@ -7,10 +8,12 @@ namespace GeneXus.Services.Log
 	public static class GXLogService
 	{
 		private static string AZURE_APPLICATION_INSIGHTS_LOG = "AZUREAPPLICATIONINSIGHTS";
+		private static string OPENTELEMETRY_SERVICE = "Observability";
 		public static ILoggerFactory GetLogFactory()
 		{		
-			Config.GetValueOf("LOG_OUTPUT", out string logProvider);		
-			if (logProvider == AZURE_APPLICATION_INSIGHTS_LOG)
+			Config.GetValueOf("LOG_OUTPUT", out string logProvider);
+			GXService providerService = GXServices.Instance?.Get(OPENTELEMETRY_SERVICE);
+			if ((logProvider == AZURE_APPLICATION_INSIGHTS_LOG) || (providerService != null && providerService.Name.Equals("AZUREMONITOR")))
 				return GeneXus.Services.Log.AzureAppInsightsLogProvider.GetLoggerFactory();
 			else
 				return null;
