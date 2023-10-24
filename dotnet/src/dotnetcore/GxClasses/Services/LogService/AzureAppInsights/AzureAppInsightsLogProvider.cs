8587ba557056732b45965958133cadbd71898535
Claudia Murialdo <cmurialdo@genexus.com>
2023-10-24 11:32:29 -0300

    Cherry pick branch 'genexuslabs:LoggingAPIImpl_Update' into beta

--- dotnet/src/dotnetcore/GxClasses/Services/LogService/AzureAppInsights/AzureAppInsightsLogProvider.cs
+++ dotnet/src/dotnetcore/GxClasses/Services/LogService/AzureAppInsights/AzureAppInsightsLogProvider.cs
@@ -8,9 +8,38 @@ namespace GeneXus.Services.Log
 	public class AzureAppInsightsLogProvider : ILoggerFactory
 	{
 		private static string APPLICATIONINSIGHTS_CONNECTION_STRING = "APPLICATIONINSIGHTS_CONNECTION_STRING";
-		private static string OPENTELEMETRY_SERVICE = "Observability";
 		public static ILoggerFactory loggerFactory;
-		
+
+		public static ILoggerFactory GetAzureMonitorLoggerFactory()
+		{
+			string appInsightsConnection = Environment.GetEnvironmentVariable(APPLICATIONINSIGHTS_CONNECTION_STRING);
+			try
+			{
+
+				if (appInsightsConnection != null)
+				{
+					loggerFactory = LoggerFactory.Create(builder =>
+					{
+						builder.AddOpenTelemetry(options =>
+						{
+							options.AddAzureMonitorLogExporter(o => o.ConnectionString = appInsightsConnection);
+							options.AddConsoleExporter();
+						});
+					});
+				}
+				else
+				{
+					throw new ArgumentNullException("APPLICATIONINSIGHTS_CONNECTION_STRING", "Application Insight Log could not be initialized due to missing APPLICATIONINSIGHTS_CONNECTION_STRING environment variable.");
+				}
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+
+			return loggerFactory;
+		}
+
 		public static ILoggerFactory GetLoggerFactory()
 		{
 			string appInsightsConnection = Environment.GetEnvironmentVariable(APPLICATIONINSIGHTS_CONNECTION_STRING);
@@ -19,28 +48,13 @@ namespace GeneXus.Services.Log
 
 				if (appInsightsConnection != null)
 				{
-					GXService providerService = GXServices.Instance?.Get(OPENTELEMETRY_SERVICE);
-					if (providerService != null && providerService.Name.Equals("AZUREMONITOR"))
-					{
-						loggerFactory = LoggerFactory.Create(builder =>
-						{
-							builder.AddOpenTelemetry(options =>
-							{
-								options.AddAzureMonitorLogExporter(o => o.ConnectionString = appInsightsConnection);
-								options.AddConsoleExporter();
-							});
-						});
-					}
-					else
-					{
-						loggerFactory = LoggerFactory.Create(builder => builder.AddApplicationInsights(
+					loggerFactory = LoggerFactory.Create(builder => builder.AddApplicationInsights(
 
-							configureTelemetryConfiguration: (config) =>
-							config.ConnectionString = appInsightsConnection,
-							configureApplicationInsightsLoggerOptions: (options) => { }
-							)
-						);
-					}
+						configureTelemetryConfiguration: (config) =>
+						config.ConnectionString = appInsightsConnection,
+						configureApplicationInsightsLoggerOptions: (options) => { }
+						)
+					);
 				}
 				else
 				{
