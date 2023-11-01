89f1c080b2efec303cd4521592d486981838dbd5
Claudia Murialdo <cmurialdo@genexus.com>
2023-11-01 13:18:54 -0300

    Replace the base class 'DictionaryBase' of 'JObject' with 'OrderedDictionary,' which implements the same logic as intended by 'JObject' for preserving the order of properties when serializing elements.

--- dotnet/src/dotnetcore/GxClasses/Domain/JObject.cs
+++ dotnet/src/dotnetcore/GxClasses/Domain/JObject.cs
@@ -1,10 +1,3 @@
-#region License, Terms and Conditions
-//
-// Jayrock - A JSON-RPC implementation for the Microsoft .NET Framework
-// Written by Atif Aziz (atif.aziz@skybow.com)
-// Copyright (c) Atif Aziz. All rights reserved.
-//
-// This library is free software; you can redistribute it and/or modify it under
 // the terms of the GNU Lesser General Public License as published by the Free
 // Software Foundation; either version 2.1 of the License, or (at your option)
 // any later version.
@@ -12,44 +5,19 @@
 // This library is distributed in the hope that it will be useful, but WITHOUT
 // ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 // FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-// details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with this library; if not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
-//
-#endregion
-
+using System;
+using System.Collections;
+using System.Collections.Specialized;
+using GeneXus.Utils;
 namespace GeneXus.Application
 {
-    #region Imports
-
-    using System;
-    using System.Collections;
-	using GeneXus.Utils;
-
-	#endregion
-
-	/// <summary>
-	/// An unordered collection of name/value pairs.
-	/// </summary>
-	/// <remarks>
-	/// <para>
-	/// Althought the collection should be considered unordered by the user, 
 	/// the implementation does internally try to remember the order in which 
 	/// the keys were added in order facilitate human-readability as in when
 	/// an instance is rendered as text.</para>
-	/// <para>
-	/// Public Domain 2002 JSON.org, ported to C# by Are Bjolseth (teleplan.no)
-	/// and re-adapted by Atif Aziz (www.raboof.com)</para>
-	/// </remarks>
 
 	[ Serializable ]
-    internal class JObject : DictionaryBase
+    internal class JObject : OrderedDictionary
     {
-        private ArrayList _nameIndexList;
-        [ NonSerialized ] private IList _readOnlyNameIndexList;
-
         public JObject() {}
 
         /// <summary>
@@ -63,33 +31,16 @@ namespace GeneXus.Application
                 if (entry.Key == null)
                     throw new Exception("InvalidMemberException");
 
-                InnerHashtable.Add(entry.Key.ToString(), entry.Value);
+				base.Add(entry.Key.ToString(), entry.Value);
             }
 
-            _nameIndexList = new ArrayList(members.Keys);
-        }
-
-        public virtual object this[string key]
-        {
-            get { return InnerHashtable[key]; }
-            set { Put(key, value); }
-        }
+         }
 
         public virtual bool HasMembers
         {
             get { return Count > 0; }
         }
 
-        private ArrayList NameIndexList
-        {
-            get
-            {
-                if (_nameIndexList == null)
-                    _nameIndexList = new ArrayList();
-
-                return _nameIndexList;
-            }
-        }
 
         /// <summary>
         /// Accumulate values under a key. It is similar to the Put method except
@@ -104,7 +55,7 @@ namespace GeneXus.Application
             if (name == null)
                 throw new ArgumentNullException(nameof(name));
 
-            object current = InnerHashtable[name];
+            object current = base[name];
 
             if (current == null)
             {
@@ -120,10 +71,12 @@ namespace GeneXus.Application
                 }
                 else
                 {
-                    values = new JArray();
-                    values.Add(current);
-                    values.Add(value);
-                    Put(name, values);
+					values = new JArray
+					{
+						current,
+						value
+					};
+					Put(name, values);
                 }
             }
 
@@ -141,59 +94,19 @@ namespace GeneXus.Application
                 throw new ArgumentNullException(nameof(name));
 
             if (value != null)
-                Dictionary[name] = value;
+                this[name] = value;
             else 
                 Remove(name);
-
+			
             return this;
-        }
-
-        public virtual bool Contains(string name)
-        {
-            if (name == null)
-                throw new ArgumentNullException(nameof(name));
-
-            return Dictionary.Contains(name);
-        }
-
-        public virtual void Remove(string name)
-        {
-            if (name == null)
-                throw new ArgumentNullException(nameof(name));
-
-            Dictionary.Remove(name);
         }
 
         public virtual ICollection Names
         {
             get
             {
-                if (_readOnlyNameIndexList == null)
-                    _readOnlyNameIndexList = (IList)NameIndexList.Clone();
-
-                return _readOnlyNameIndexList;
+				return base.Keys;
             }
-        }
-
-        /// <summary>
-        /// Produce a JArray containing the names of the elements of this
-        /// JObject.
-        /// </summary>
-
-        public virtual JArray GetNamesArray()
-        {
-            JArray names = new JArray();
-            ListNames(names);
-            return names;
-        }
-
-        public virtual void ListNames(IList list)
-        {
-            if (list == null)
-                throw new ArgumentNullException(nameof(list));
-
-            foreach (string name in NameIndexList)
-                list.Add(name);
         }
 
         /// <summary>
@@ -205,55 +118,6 @@ namespace GeneXus.Application
 			return JSONHelper.WriteJSON(this);
 		}
 
-        protected override void OnValidate(object key, object value)
-        {
-            base.OnValidate(key, value);
 
-            if (key == null)
-                throw new ArgumentNullException(nameof(key));
-
-            if (!(key is string))
-                throw new ArgumentOutOfRangeException(nameof(key));
-        }
-
-        protected override void OnInsert(object key, object value)
-        {
-            //
-            // NOTE: OnInsert leads one to believe that keys are ordered in the
-            // base dictionary in that they can be inserted somewhere in the
-            // middle. However, the base implementation only calls OnInsert
-            // during the Add operation, so we known it is safe here to simply
-            // add the new key at the end of the name list.
-            //
-
-            NameIndexList.Add(key);
-        }
-
-        protected override void OnSet(object key, object oldValue, object newValue)
-        {
-            //
-            // NOTE: OnSet is called when the base dictionary is modified via
-            // the indexer. We need to trap this and detect when a new key is
-            // being added via the indexer. If the old value is null for the
-            // key, then there is a big chance it is a new key. But just to be
-            // sure, we also check out key index if it does not already exist.
-            // Finally, we just delegate to OnInsert. In effect, we're
-            // converting OnSet to OnInsert where needed. Ideally, the base
-            // implementation would have done this for.
-            //
-
-            if (oldValue == null && !NameIndexList.Contains(key))
-                OnInsert(key, newValue);
-        }
-
-        protected override void OnRemove(object key, object value)
-        {
-            NameIndexList.Remove(key);
-        }
-
-        protected override void OnClear()
-        {
-            NameIndexList.Clear();
-        }
     }
 }
