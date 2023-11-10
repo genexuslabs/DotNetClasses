1b418c8bf34fec8559e67ff0bad0559cf1d76a65
Claudia Murialdo <cmurialdo@genexus.com>
2023-11-10 16:17:53 -0300

    Filter requests accessing TMPMEDIA_DIR folder.

--- dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
+++ dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
@@ -370,14 +370,21 @@ namespace GeneXus.Application
 			if (File.Exists(rewriteFile))
 				AddRewrite(app, rewriteFile, baseVirtualPath);
 
+			string tempMediaDir = string.Empty;
+			if (Config.GetValueOf("TMPMEDIA_DIR", out string mediaPath) && !PathUtil.IsAbsoluteUrlOrAnyScheme(mediaPath))
+			{
+				tempMediaDir = mediaPath;
+			}
 			app.UseStaticFiles(new StaticFileOptions()
 			{
 				FileProvider = new PhysicalFileProvider(LocalPath),
 				RequestPath = new PathString($"{baseVirtualPath}"),
 				OnPrepareResponse = s =>
 				{
-					var path = s.Context.Request.Path;
-					if (path.HasValue &&  path.Value.IndexOf($"/{APP_SETTINGS}", StringComparison.OrdinalIgnoreCase)>=0)
+					PathString path = s.Context.Request.Path;
+					bool appSettingsPath = path.HasValue && path.Value.IndexOf($"/{APP_SETTINGS}", StringComparison.OrdinalIgnoreCase) >= 0;
+					bool tempMediaPath = path.StartsWithSegments($"{baseVirtualPath}/{tempMediaDir}", StringComparison.OrdinalIgnoreCase);
+					if (appSettingsPath || tempMediaPath)
 					{
 						s.Context.Response.StatusCode = 401;
 						s.Context.Response.Body = Stream.Null;
