8a79d145c9443d4de69820a082d5dc236dd59753
Claudia Murialdo <cmurialdo@genexus.com>
2023-12-07 15:15:17 -0300

    Cherry pick branch 'genexuslabs:logging-loader' into beta

--- dotnet/test/benchmarks/LogTest/LogTest.cs
+++ dotnet/test/benchmarks/LogTest/LogTest.cs
@@ -1,13 +1,18 @@
+using System.Linq;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 using GeneXus.Diagnostics;
 using Xunit;
 namespace BenchmarkTest
 {
+#if DEBUG
+	[Config(typeof(CustomBenchmarkConfig))]
+#endif
 	public class LoggerBenchmark
 	{
-		[Params(1000)]
+		[Params(100)]
 		public int N;
 		const int Debug = 5;
 
@@ -20,24 +25,29 @@ namespace BenchmarkTest
 			}
 		}
 	}
+	public class CustomBenchmarkConfig : ManualConfig
+	{
+		public CustomBenchmarkConfig()
+		{
+			WithOptions(ConfigOptions.DisableOptimizationsValidator);
+		}
+	}
 	public class PerformanceTests
 	{
-#if RELEASE
 		const double MAX_NANOSECONDS = 150000; 
 		[Fact]
 		public void RunLoggerBenchmark()
 		{
 			Summary summary = BenchmarkRunner.Run<LoggerBenchmark>();
-			Assert.NotEmpty(summary.Reports);
+			Assert.Single(summary.Reports);
+
 			foreach (BenchmarkReport report in summary.Reports)
 			{
 				Assert.NotEmpty(report.AllMeasurements);
-				foreach (Measurement runMeasure in report.AllMeasurements)
-				{
-					Assert.InRange(runMeasure.Nanoseconds, low: 0, high: MAX_NANOSECONDS);
-				}
+				Measurement runMeasure = report.AllMeasurements.FirstOrDefault();
+				Assert.InRange(runMeasure.Nanoseconds, low: 0, high: MAX_NANOSECONDS);
+				
 			}
 		}
-#endif
 	}
 }
