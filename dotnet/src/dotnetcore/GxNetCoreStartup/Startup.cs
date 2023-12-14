4ba973a76c1bd14e30c1728a7097d24ff3f602dd
Claudia Murialdo <cmurialdo@genexus.com>
2023-12-14 16:37:27 -0300

    Remove the default DistributedCache middleware, as distributed memory is configured with Redis.

--- dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
+++ dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
@@ -170,7 +170,6 @@ namespace GeneXus.Application
 			{
 				options.AllowSynchronousIO = true;
 			});
-			services.AddDistributedMemoryCache();
 			services.AddLogging(builder => builder.AddConsole());
 			services.Configure<FormOptions>(options =>
 			{
