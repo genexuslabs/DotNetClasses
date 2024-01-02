49e69a0e7f8b2c72b517cdde03a1396609c5a763
Tomás Sexenian <99925035+tomas-sexenian@users.noreply.github.com>
2024-01-02 12:24:37 -0300

    Remove the ; at the end of httpclient variable (#916)
    Issue: 106225

--- dotnet/src/dotnetframework/GxClasses/Domain/GxHttpClient.cs
+++ dotnet/src/dotnetframework/GxClasses/Domain/GxHttpClient.cs
@@ -51,7 +51,7 @@ namespace GeneXus.Http.Client
 			Boundary = "----------------------------" + DateTime.Now.Ticks.ToString("x");
 			ContentType = $"multipart/form-data; boundary={Boundary}";
 			FormdataSeparator = "\r\n";
-			FormdataTemplate = "--" + Boundary + "\r\nContent-Disposition: form-data; name=\"{0}\";\r\n\r\n{1}";
+			FormdataTemplate = "--" + Boundary + "\r\nContent-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
 			Boundarybytes = Encoding.ASCII.GetBytes($"\r\n--{Boundary}\r\n");
 			EndBoundaryBytes = Encoding.ASCII.GetBytes($"\r\n--{Boundary}--");
 			HeaderTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\n" + "Content-Type: {2}\r\n\r\n";
