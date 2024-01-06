f8dd870fd894e463993b240a9dff08236383cb02
sjuarezgx <sjuarez@genexus.com>
2024-01-06 17:47:27 -0300

    Cherry pick branch 'genexuslabs:Otel_AddDefaultMetrics' into beta

--- dotnet/src/dotnetcore/Providers/OpenTelemetry/OpenTelemetry/OpenTelemetryProvider.cs
+++ dotnet/src/dotnetcore/Providers/OpenTelemetry/OpenTelemetry/OpenTelemetryProvider.cs
@@ -25,7 +25,6 @@ namespace GeneXus.OpenTelemetry.OpenTelemetry
 			{
 				metricsProviderBuilder
 				.AddOtlpExporter()
-				.AddConsoleExporter()
 				.AddGxMeterAspNetInstrumentation();
 			});
 			return true;
