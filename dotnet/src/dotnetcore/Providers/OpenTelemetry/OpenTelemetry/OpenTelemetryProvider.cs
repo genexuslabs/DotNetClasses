1597338cedaab8703e330b9fa0efac512169521c
sjuarezgx <sjuarez@genexus.com>
2024-01-27 12:37:42 -0300

    Avoid generating Otel Traces and metrics through Env Var

--- dotnet/src/dotnetcore/Providers/OpenTelemetry/OpenTelemetry/OpenTelemetryProvider.cs
+++ dotnet/src/dotnetcore/Providers/OpenTelemetry/OpenTelemetry/OpenTelemetryProvider.cs
@@ -4,28 +4,39 @@ using Microsoft.Extensions.DependencyInjection;
 using OpenTelemetry.Trace;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
+using System;
+using Microsoft.IdentityModel.Tokens;
 
 namespace GeneXus.OpenTelemetry.OpenTelemetry
 {
 	public class OpenTelemetryProvider : IOpenTelemetryProvider
 	{
+		const string OTEL_METRICS_EXPORTER = "OTEL_METRICS_EXPORTER";
+		const string OTEL_TRACES_EXPORTER = "OTEL_TRACES_EXPORTER";
 		public OpenTelemetryProvider(GXService s)
 		{
 		}
 
 		public bool InstrumentAspNetCoreApplication(IServiceCollection services)
 		{
-			services.AddOpenTelemetry().WithTracing(tracerProviderBuilder =>
-			{
-				tracerProviderBuilder
-				.AddOtlpExporter()
-				.AddGxAspNetInstrumentation();
-			});
-			services.AddOpenTelemetry().WithMetrics(metricsProviderBuilder =>
-			{
-				metricsProviderBuilder
-				.AddGxMeterAspNetInstrumentation();
-			});
+			string envvar = Environment.GetEnvironmentVariable(OTEL_TRACES_EXPORTER);
+			if (envvar.IsNullOrEmpty() || !envvar.ToLower().Equals("none")) {
+				services.AddOpenTelemetry().WithTracing(tracerProviderBuilder =>
+				{
+					tracerProviderBuilder
+					.AddOtlpExporter()
+					.AddGxAspNetInstrumentation();
+				});
+			}
+
+			envvar = Environment.GetEnvironmentVariable(OTEL_METRICS_EXPORTER);
+			if (envvar.IsNullOrEmpty() || !envvar.ToLower().Equals("none")) {
+				services.AddOpenTelemetry().WithMetrics(metricsProviderBuilder =>
+				{
+					metricsProviderBuilder
+					.AddGxMeterAspNetInstrumentation();
+				});
+			}
 			return true;
 		}
 	}
