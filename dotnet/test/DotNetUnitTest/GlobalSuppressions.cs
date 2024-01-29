44704683711f1bb9e74ee7d0a371f2fa91c346bf
Claudia Murialdo <cmurialdo@genexus.com>
2024-01-29 14:57:17 -0300

    Suppress warning CA1825

---
+++ dotnet/test/DotNetUnitTest/GlobalSuppressions.cs
@@ -0,0 +1,31 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestCopyMethod")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestCopyPrivateMethod")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestDeleteFile")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestDeleteFilePrivate")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestDirectoryExists")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestDirectoryNotExists")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestDownloadMethod")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestEmptyFolder")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestFileExists")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestFileNotExists")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestGetMethod")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestGetObjectName")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestMultimediaUpload")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestUploadAndCopyDefault")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestUploadAndCopyMixed")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestUploadAndCopyPrivate")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestUploadAndCopyPrivateToPublic")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestUploadAndCopyPublic")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestUploadAndCopyPublicToPrivate")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestUploadDefaultAttributeMethod")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestUploadDefaultMethod")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestUploadPrivateMethod")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestUploadPrivateSubfolderMethod")]
+[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.", Justification = "<Pending>", Scope = "member", Target = "~M:UnitTesting.ExternalProviderTest.TestUploadPublicMethod")]
