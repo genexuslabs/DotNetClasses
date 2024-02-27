47e6c87ae3bda4b51bb9f9a8f5a6b09a6af78104
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2024-02-27 10:20:21 -0300

    Unit test for Forwarded Headers Middleware (#967)
    * Enabled Forwarded Headers Middleware.
    
    * Consider the scheme in the unit test.
    
    * Remove Forward headers Middleware as it is enabled by default when ASPNETCORE_FORWARDEDHEADERS_ENABLED environment variable is true.

--- dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
+++ dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
@@ -18,6 +18,7 @@ using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.AspNetCore.Routing;
