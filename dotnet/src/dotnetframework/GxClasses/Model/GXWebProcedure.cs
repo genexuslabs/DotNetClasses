8198c4cd4a069576ef6905005aff46f62916eb51
AlejandroP <apanizza@genexus.com>
2024-03-02 02:44:19 -0300

    - isDataProvider property was not inherited in NET 4.X . Created a different one.

--- dotnet/src/dotnetframework/GxClasses/Model/GXWebProcedure.cs
+++ dotnet/src/dotnetframework/GxClasses/Model/GXWebProcedure.cs
@@ -55,7 +55,7 @@ namespace GeneXus.Procedure
 			}
 		}
 		public void setContextReportHandler()
-		{
+		{	
 
 			oldReportHandler = null;
 			reportHandler = context.reportHandler;
@@ -92,6 +92,16 @@ namespace GeneXus.Procedure
 			outputType = fileType.Trim();
 		}
 
+#if !NETCORE
+
+        protected bool _isDP;
+
+		public bool IsDataProvider
+		{
+			set { _isDP = value; }
+			get { return _isDP; }
+		}
+#endif
 		protected override void sendCacheHeaders()
 		{
 			
