3f27d2a1cf40e4b71e1a7d79c677dd4e1db03338
sjuarezgx <sjuarez@genexus.com>
2024-03-12 12:56:14 -0300

    Cherry pick branch 'genexuslabs:Complete_CustomSpansOtel_Impl' into beta

--- dotnet/src/dotnetcore/Providers/OpenTelemetry/Diagnostics/GXOtel.Diagnostics/OtelTracer.cs
+++ dotnet/src/dotnetcore/Providers/OpenTelemetry/Diagnostics/GXOtel.Diagnostics/OtelTracer.cs
@@ -1,35 +1,103 @@
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using GeneXus.Application;
+using System.Linq;
 using GeneXus.Attributes;
+using GeneXus.Services.OpenTelemetry;
 
 namespace GeneXus.OpenTelemetry.Diagnostics
 {
 	[GXApi]
 	public class OtelTracer
 	{
+		internal static ActivitySource activitySource;
+		internal static ActivitySource ActivitySource
+		{
+			get
+			{
+				if (activitySource == null)
+				{
+					activitySource = string.IsNullOrEmpty(OpenTelemetryService.GX_ACTIVITY_SOURCE_VERSION) ? new(OpenTelemetryService.GX_ACTIVITY_SOURCE_NAME) : new(OpenTelemetryService.GX_ACTIVITY_SOURCE_NAME, OpenTelemetryService.GX_ACTIVITY_SOURCE_VERSION);
+				}
+				return activitySource;
+			}
+		}
 		public enum SpanType
 		{
-			Client,
-			Consumer,
 			Internal,
+			Server,
+			Client,
 			Producer,
-			Server
+			Consumer
+		}
+
+		public OtelSpan CreateSpan(string name)
+		{
+			//Returns null when the are no listeners.
+			Activity activity = ActivitySource.StartActivity(name);
+			if (activity != null) 
+				return new OtelSpan(activity);
+			return null;
 		}
 
 		public OtelSpan CreateSpan(string name, SpanType kind)
 		{
-			Activity activity = GXBaseObject.ActivitySource.StartActivity(name, (ActivityKind)kind);
+			Activity activity = ActivitySource.StartActivity(name, (ActivityKind)kind);
+				if (activity != null)
+				return new OtelSpan(activity);
+			else
+				return null;
+		}
+
+		public OtelSpan CreateSpan(string name, GXTraceContext gxTraceContext, SpanType spanType)
+		{
+			Activity activity = ActivitySource.StartActivity(name,
+														 kind: (ActivityKind)spanType,
+														 parentContext: gxTraceContext.ActivityContext);
+			return new OtelSpan(activity);
+
+		}
+	
+		public OtelSpan CreateSpan(string name, GXTraceContext gxTraceContext, SpanType spanType, IList<GXSpanContext> gxSpanContexts)
+		{
+			//https://learn.microsoft.com/en-us/dotnet/core/diagnostics/distributed-tracing-instrumentation-walkthroughs#optional-links
+			List<ActivityContext> contexts = new List<ActivityContext>();
+
+			foreach (GXSpanContext gxSpanContext in gxSpanContexts)
+			{
+				ActivityContext context = gxSpanContext.ActivityContext;
+				contexts.Add(context);
+			}
+
+			Activity activity = ActivitySource.StartActivity(name,
+													 kind: (ActivityKind)spanType,
+													 parentContext: gxTraceContext.ActivityContext,
+													 links: contexts.Select(ctx => new ActivityLink(ctx)));
 			return new OtelSpan(activity);
 		}
 
-		public OtelSpan GetCurrent()
+		public OtelSpan CreateSpan(string name, GXTraceContext gxTraceContext, SpanType spanType, IList<GXSpanContext> gxSpanContexts, DateTime dateTime)
 		{
-			return new OtelSpan(Activity.Current);
+			//https://learn.microsoft.com/en-us/dotnet/core/diagnostics/distributed-tracing-instrumentation-walkthroughs#optional-links
+			List<ActivityContext> contexts = new List<ActivityContext>();
+
+			foreach (GXSpanContext gxSpanContext in gxSpanContexts)
+			{
+				ActivityContext context = gxSpanContext.ActivityContext;
+				contexts.Add(context);
+			}
+
+			Activity activity = ActivitySource.StartActivity(name,
+													 kind: (ActivityKind)spanType,
+													 parentContext: gxTraceContext.ActivityContext,
+													 links: contexts.Select(ctx => new ActivityLink(ctx)),
+													 startTime:dateTime);
+			return new OtelSpan(activity);
 		}
 
-		public bool HasListeners()
+		public static OtelSpan GetCurrent()
 		{
-			return GXBaseObject.ActivitySource.HasListeners();
+			return new OtelSpan(Activity.Current);
 		}
 	}
 }
