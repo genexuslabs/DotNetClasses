cd6c42ebb9311e2291d28862eab9e0cae84d1715
Claudia Murialdo <cmurialdo@genexus.com>
2024-03-20 15:25:16 -0300

    Prepare for async calls to httpclient. Enable Microsoft.VisualStudio.Threading.Analyzers

---
+++ dotnet/src/dotnetcore/Providers/Messaging/GXAmazonSQS/GlobalSuppressions.cs
@@ -0,0 +1,15 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "VSTHRD002:Avoid problematic synchronous waits", Justification = "<Pending>", Scope = "member", Target = "~M:GeneXus.Messaging.Queue.AWSQueue.Clear(System.Boolean@)")]
+[assembly: SuppressMessage("Usage", "VSTHRD002:Avoid problematic synchronous waits", Justification = "<Pending>", Scope = "member", Target = "~M:GeneXus.Messaging.Queue.AWSQueue.DeleteMessage(GeneXus.Messaging.Common.SimpleQueueMessage,System.Boolean@)~GeneXus.Messaging.Common.MessageQueueResult")]
+[assembly: SuppressMessage("Usage", "VSTHRD002:Avoid problematic synchronous waits", Justification = "<Pending>", Scope = "member", Target = "~M:GeneXus.Messaging.Queue.AWSQueue.GetQueueLength(System.Boolean@)~System.Int32")]
+[assembly: SuppressMessage("Usage", "VSTHRD002:Avoid problematic synchronous waits", Justification = "<Pending>", Scope = "member", Target = "~M:GeneXus.Messaging.Queue.AWSQueue.RemoveMessages(System.Collections.Generic.IList{GeneXus.Messaging.Common.SimpleQueueMessage},System.Boolean@)~System.Collections.Generic.IList{GeneXus.Messaging.Common.MessageQueueResult}")]
+[assembly: SuppressMessage("Usage", "VSTHRD002:Avoid problematic synchronous waits", Justification = "<Pending>", Scope = "member", Target = "~M:GeneXus.Messaging.Queue.AWSQueue.RetrieveMessages(System.Boolean@,GeneXus.Messaging.Common.MessageQueueOptions)~System.Collections.Generic.IList{GeneXus.Messaging.Common.SimpleQueueMessage}")]
+[assembly: SuppressMessage("Usage", "VSTHRD002:Avoid problematic synchronous waits", Justification = "<Pending>", Scope = "member", Target = "~M:GeneXus.Messaging.Queue.AWSQueue.SendMessage(GeneXus.Messaging.Common.SimpleQueueMessage,GeneXus.Messaging.Common.MessageQueueOptions,System.Boolean@)~GeneXus.Messaging.Common.MessageQueueResult")]
+[assembly: SuppressMessage("Usage", "VSTHRD002:Avoid problematic synchronous waits", Justification = "<Pending>", Scope = "member", Target = "~M:GeneXus.Messaging.Queue.AWSQueue.SendMessage(GeneXus.Messaging.Common.SimpleQueueMessage,System.Boolean@)~GeneXus.Messaging.Common.MessageQueueResult")]
+[assembly: SuppressMessage("Usage", "VSTHRD002:Avoid problematic synchronous waits", Justification = "<Pending>", Scope = "member", Target = "~M:GeneXus.Messaging.Queue.AWSQueue.SendMessages(System.Collections.Generic.IList{GeneXus.Messaging.Common.SimpleQueueMessage},GeneXus.Messaging.Common.MessageQueueOptions,System.Boolean@)~System.Collections.Generic.IList{GeneXus.Messaging.Common.MessageQueueResult}")]
