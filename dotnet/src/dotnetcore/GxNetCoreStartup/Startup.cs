9ec3167bb537780d536388041c4df280b274480b
Claudia Murialdo <cmurialdo@genexus.com>
2024-05-16 11:43:14 -0300

    Enable support for configuring Before Connect Event handling for the Session provider Datastore.
    (cherry picked from commit dcee98e32a6ab085832e3165dd8d03c05b89f045)
    
    # Conflicts:
    #	dotnet/DotNetStandardClasses.sln

--- dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
+++ dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
@@ -27,6 +27,8 @@ using Microsoft.AspNetCore.Rewrite;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.SqlServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
@@ -207,9 +209,10 @@ namespace GeneXus.Application
 			});
 			ISessionService sessionService = GXSessionServiceFactory.GetProvider();
 
+			services.AddHttpContextAccessor();
 			if (sessionService != null)
 				ConfigureSessionService(services, sessionService);
-			services.AddHttpContextAccessor();
+
 			services.AddSession(options =>
 			{
 				options.IdleTimeout = TimeSpan.FromMinutes(Preferences.SessionTimeout);
@@ -419,14 +422,38 @@ namespace GeneXus.Application
 			}
 			else if (sessionService is GxDatabaseSession)
 			{
-				services.AddDistributedSqlServerCache(options =>
+
+				if (Preferences.IsBeforeConnectEventConfigured())
 				{
-					GXLogging.Info(log, $"Using SQLServer for Distributed session, ConnectionString:{sessionService.ConnectionString}, SchemaName: {sessionService.Schema}, TableName: {sessionService.TableName}");
-					options.ConnectionString = sessionService.ConnectionString;
-					options.SchemaName = sessionService.Schema;
-					options.TableName = sessionService.TableName;
-					options.DefaultSlidingExpiration = TimeSpan.FromMinutes(sessionService.SessionTimeout);
-				});
+					services.AddTransient<CacheResolver>(_ => connectionString =>
+					{
+						GXLogging.Info(log, $"Using SQLServer for request scoped Distributed session, ConnectionString:{sessionService.ConnectionString}, SchemaName: {sessionService.Schema}, TableName: {sessionService.TableName}");
+						Action<SqlServerCacheOptions> cacheConfigOptions = options =>
+						{
+							options.ConnectionString = connectionString;
+							options.SchemaName = sessionService.Schema;
+							options.TableName = sessionService.TableName;
+							options.DefaultSlidingExpiration = TimeSpan.FromMinutes(sessionService.SessionTimeout);
+						};
+						services.AddOptions();
+						services.Configure(cacheConfigOptions);
+						services.AddTransient<SqlServerCache>();
+						return services.BuildServiceProvider().GetService<SqlServerCache>();
+					});
+					services.AddTransient<IDistributedCache, CustomCacheProvider>();
+				}
+				else
+				{
+
+					services.AddDistributedSqlServerCache(options =>
+					{
+						GXLogging.Info(log, $"Using SQLServer for Distributed session, ConnectionString:{sessionService.ConnectionString}, SchemaName: {sessionService.Schema}, TableName: {sessionService.TableName}");
+						options.ConnectionString = sessionService.ConnectionString;
+						options.SchemaName = sessionService.Schema;
+						options.TableName = sessionService.TableName;
+						options.DefaultSlidingExpiration = TimeSpan.FromMinutes(sessionService.SessionTimeout);
+					});
+				}
 			}
 		}
 		public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env, ILoggerFactory loggerFactory)
@@ -459,6 +486,10 @@ namespace GeneXus.Application
 			}
 			app.UseRouting();
 			app.UseCookiePolicy();
+			if (Preferences.IsBeforeConnectEventConfigured())
+			{
+				app.UseMiddleware<EnableCustomSessionStoreMiddleware>();
+			}
 			app.UseSession();
 			app.UseStaticFiles();
 			ConfigureCors(app);
@@ -682,6 +713,20 @@ namespace GeneXus.Application
 				}
 			}
 			await Task.CompletedTask;
+		}
+	}
+	internal class EnableCustomSessionStoreMiddleware
+	{
+		private readonly RequestDelegate _next;
+
+		public EnableCustomSessionStoreMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task Invoke(HttpContext context, IDistributedCache distributedCache)
+		{
+			await _next(context);
 		}
 	}
 	public class EnableRequestRewindMiddleware
