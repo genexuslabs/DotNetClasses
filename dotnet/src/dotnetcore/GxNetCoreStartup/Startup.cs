f231a163056772eee18e39632880d6f8ecd44d19
Claudia Beatriz Murialdo Garrone <c.murialdo@globant.com>
2024-08-27 15:51:45 -0300

    Grouped several lines of code into a new function, RegisterControllerAssemblies.
    (cherry picked from commit 495aa82f14c5086196e8a6f6a93db07a83320e1b)

--- dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
+++ dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
@@ -177,34 +177,9 @@ namespace GeneXus.Application
 		{
 			OpenTelemetryService.Setup(services);
 
-			IMvcBuilder mvcBuilder;
-			if (RestAPIHelpers.ServiceAsController() && !string.IsNullOrEmpty(VirtualPath))
-				mvcBuilder = services.AddControllers(options =>
-				{
-					options.Conventions.Add(new SetRoutePrefix(new RouteAttribute(VirtualPath)));
-				});
-			else
-				mvcBuilder=services.AddControllers();
+			services.AddMvc(option => option.EnableEndpointRouting = false);
 
-
-			mvcBuilder.AddJsonOptions(options => options.JsonSerializerOptions.PropertyNameCaseInsensitive = false);
-			mvcBuilder.ConfigureApiBehaviorOptions(options =>
-			 {
-				 options.InvalidModelStateResponseFactory = context =>
-				 {
-					 return new CustomBadRequestObjectResult(context);
-				 };
-			 });
-
-
-			mvcBuilder = services.AddMvc(option => option.EnableEndpointRouting = false);
-			
-			if (RestAPIHelpers.ServiceAsController())
-			{
-				RegisterRestServices(mvcBuilder);
-				RegisterApiServices(mvcBuilder, gxRouting);
-			}
-			RegisterNativeServices(mvcBuilder);
+			RegisterControllerAssemblies(services);
 
 			services.Configure<KestrelServerOptions>(options =>
 			{
@@ -292,6 +267,36 @@ namespace GeneXus.Application
 				});
 			}
 			DefineCorsPolicy(services);
+		}
+
+		private void RegisterControllerAssemblies(IServiceCollection services)
+		{
+			IMvcBuilder mvcBuilder;
+			if (RestAPIHelpers.ServiceAsController() && !string.IsNullOrEmpty(VirtualPath))
+				mvcBuilder = services.AddControllers(options =>
+				{
+					options.Conventions.Add(new SetRoutePrefix(new RouteAttribute(VirtualPath)));
+				});
+			else
+				mvcBuilder = services.AddControllers();
+
+
+			mvcBuilder.AddJsonOptions(options => options.JsonSerializerOptions.PropertyNameCaseInsensitive = false);
+			mvcBuilder.ConfigureApiBehaviorOptions(options =>
+			{
+				options.InvalidModelStateResponseFactory = context =>
+				{
+					return new CustomBadRequestObjectResult(context);
+				};
+			});
+
+			if (RestAPIHelpers.ServiceAsController())
+			{
+				RegisterRestServices(mvcBuilder);
+				RegisterApiServices(mvcBuilder, gxRouting);
+			}
+			RegisterNativeServices(mvcBuilder);
+
 		}
 
 		private void RegisterNativeServices(IMvcBuilder mvcBuilder)
