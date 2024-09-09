f2aa5e913c1aae6067e9c55f290d25c79c559ad6
dmendez <dmendez@genexus.com>
2024-09-09 12:31:42 +0000

    Cherry pick branch 'genexuslabs:Issue-108639-WEB-request-tabId' into beta

--- dotnet/src/dotnetframework/GxClasses/Core/GXApplication.cs
+++ dotnet/src/dotnetframework/GxClasses/Core/GXApplication.cs
@@ -1684,9 +1684,9 @@ namespace GeneXus.Application
 			query = RemoveInternalSuffixes(query);
 			return query;
 		}
-		internal static string RemoveInternalSuffixes(string query)
+		internal static string RemoveInternalSuffix(string query, string param)
 		{
-			int idx = query.IndexOf(GXNavigationHelper.POPUP_LEVEL);
+			int idx = query.IndexOf(param);
 			if (idx == 1)
 				return "";
 			if (idx > 1)
@@ -1698,6 +1698,12 @@ namespace GeneXus.Application
 				query = query.Substring(0, idx);
 			}
 			return query;
+		}
+
+		internal static string RemoveInternalSuffixes(string query)
+		{
+			query = RemoveInternalSuffix(query, GXNavigationHelper.POPUP_LEVEL);
+			return RemoveInternalSuffix(query, GXNavigationHelper.TAB_ID);
 		}
 		private string RemoveEventPrefix(string query)
 		{
