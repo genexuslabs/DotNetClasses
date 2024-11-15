0893896c48f829a4d3ec78a33629b28628a30573
Claudia Murialdo <cmurialdo@genexus.com>
2024-11-15 20:06:22 +0000

    Cherry pick branch 'genexuslabs:embedding-data-type' into beta

--- dotnet/src/dotnetcore/Providers/AI/Model/GXAgent.cs
+++ dotnet/src/dotnetcore/Providers/AI/Model/GXAgent.cs
@@ -1,3 +1,4 @@
+using System;
 using GeneXus.Procedure;
 using GeneXus.Utils;
 using OpenAI.Chat;
@@ -6,10 +7,20 @@ namespace GeneXus.AI
 {
 	public class GXAgent : GXProcedure
 	{
-		protected string CallAssistant(string modelId, GXProperties properties, object result)
+		protected string CallAssistant(string assistant, GXProperties properties, object result)
 		{
+			ChatCompletion chatCompletion=null;
 			CallResult callResult = result as CallResult;
-			ChatCompletion chatCompletion = AgentService.AgentHandlerInstance.Assistant(modelId, string.Empty, properties).GetAwaiter().GetResult();
+			try
+			{
+				chatCompletion = AgentService.AgentHandlerInstance.Assistant(assistant, string.Empty, properties).GetAwaiter().GetResult();
+			}
+			catch (Exception ex)
+			{
+				callResult.AddMessage($"Error calling Agent {assistant}:" + ex.Message);
+				callResult.IsFail = true;
+				return null;
+			}
 			if (chatCompletion != null && chatCompletion.Content.Count > 0)
 			{
 				return chatCompletion.Content[0].Text;
