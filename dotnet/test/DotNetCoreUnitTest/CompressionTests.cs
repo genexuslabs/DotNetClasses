72bc65ca79a159aaf6889524ec1505d71618b20d
Tomás Sexenian <99925035+tomas-sexenian@users.noreply.github.com>
2024-11-25 03:18:27 +0900

    Try make tests

--- dotnet/test/DotNetCoreUnitTest/CompressionTests.cs
+++ dotnet/test/DotNetCoreUnitTest/CompressionTests.cs
@@ -3,6 +3,7 @@ using GeneXus.Utils;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace xUnitTesting
@@ -15,292 +16,131 @@ namespace xUnitTesting
 		{
 		}
 
-		private void CreateTestFiles(string rootPath, out List<string> filePaths, out List<string> dirPaths)
-		{
-			filePaths = new List<string>();
-			dirPaths = new List<string>();
-
-			// Create files in the root directory
-			for (int i = 0; i < 3; i++)
-			{
-				string filePath = Path.Combine(rootPath, $"testfile_{i}.txt");
-				File.WriteAllText(filePath, $"This is test file {i}");
-				filePaths.Add(filePath);
-			}
-
-			// Create subdirectories with files
-			for (int i = 0; i < 2; i++)
-			{
-				string dirPath = Path.Combine(rootPath, $"testdir_{i}");
-				Directory.CreateDirectory(dirPath);
-				dirPaths.Add(dirPath);
-
-				for (int j = 0; j < 2; j++)
-				{
-					string filePath = Path.Combine(dirPath, $"testfile_{i}_{j}.txt");
-					File.WriteAllText(filePath, $"This is test file {i}_{j}");
-					filePaths.Add(filePath);
-				}
-			}
-		}
-
-		private void CleanupTestFiles(List<string> filePaths, List<string> dirPaths, string rootPath)
-		{
-			foreach (string filePath in filePaths)
-			{
-				if (File.Exists(filePath))
-				{
-					File.Delete(filePath);
-				}
-			}
-
-			foreach (string dirPath in dirPaths)
-			{
-				if (Directory.Exists(dirPath))
-				{
-					Directory.Delete(dirPath, true);
-				}
-			}
-
-			if (Directory.Exists(rootPath))
-			{
-				Directory.Delete(rootPath, true);
-			}
-		}
-
 		[Fact]
 		public void TestZip()
 		{
-			string rootPath = Path.Combine(Path.GetTempPath(), "TestZip_" + Guid.NewGuid().ToString());
-			Directory.CreateDirectory(rootPath);
-
-			List<string> filePaths;
-			List<string> dirPaths;
-
-			CreateTestFiles(rootPath, out filePaths, out dirPaths);
+			string tempFolder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+			string decompressedFolder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+			string compressedFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".zip");
 
 			try
 			{
-				List<string> itemsToCompress = new List<string>();
-				itemsToCompress.AddRange(filePaths);
-				itemsToCompress.AddRange(dirPaths);
+				CreateTestFilesAndFolders(tempFolder);
 
-				string zipFilePath = Path.Combine(Path.GetTempPath(), "test.zip");
+				var filesToCompress = new List<string> { tempFolder };
 
-				bool compressResult = GXCompressor.Compress(itemsToCompress, zipFilePath, -1, ref messages);
-				Assert.True(compressResult);
+				bool compressionResult = GXCompressor.Compress(filesToCompress, compressedFile, -1, ref messages);
+				Assert.True(compressionResult, "Compression failed");
+				Assert.True(File.Exists(compressedFile), "Compressed file does not exist");
 
-				CleanupTestFiles(filePaths, dirPaths, rootPath);
-				Directory.CreateDirectory(rootPath);
+				bool decompressionResult = GXCompressor.Decompress(compressedFile, decompressedFolder, ref messages);
+				Assert.True(decompressionResult, "Decompression failed");
 
-				bool decompressResult = GXCompressor.Decompress(zipFilePath, rootPath, ref messages);
-				Assert.True(decompressResult);
-
-				foreach (string filePath in filePaths)
-				{
-					Assert.True(File.Exists(filePath));
-				}
-
-				foreach (string dirPath in dirPaths)
-				{
-					Assert.True(Directory.Exists(dirPath));
-				}
-
-				CleanupTestFiles(filePaths, dirPaths, rootPath);
-
-				if (File.Exists(zipFilePath))
-				{
-					File.Delete(zipFilePath);
-				}
+				bool directoriesAreEqual = CompareDirectories(tempFolder, decompressedFolder);
+				Assert.True(directoriesAreEqual, "Decompressed content does not match the original");
 			}
 			finally
 			{
-				if (Directory.Exists(rootPath))
+				DeleteTestFilesAndFolders(tempFolder);
+				DeleteTestFilesAndFolders(decompressedFolder);
+				if (File.Exists(compressedFile))
 				{
-					Directory.Delete(rootPath, true);
+					File.Delete(compressedFile);
 				}
+			}
+
+			bool CompareDirectories(string dir1, string dir2)
+			{
+				string[] dir1Files = Directory.GetFiles(dir1, "*", SearchOption.AllDirectories);
+				string[] dir2Files = Directory.GetFiles(dir2, "*", SearchOption.AllDirectories);
+
+				if (dir1Files.Length != dir2Files.Length)
+					return false;
+
+				Array.Sort(dir1Files);
+				Array.Sort(dir2Files);
+
+				for (int i = 0; i < dir1Files.Length; i++)
+				{
+					string relativePath1 = dir1Files[i].Substring(dir1.Length).TrimStart(Path.DirectorySeparatorChar);
+					string relativePath2 = dir2Files[i].Substring(dir2.Length).TrimStart(Path.DirectorySeparatorChar);
+
+					if (!string.Equals(relativePath1, relativePath2, StringComparison.OrdinalIgnoreCase))
+						return false;
+
+					if (!FileContentsAreEqual(dir1Files[i], dir2Files[i]))
+						return false;
+				}
+
+				return true;
+			}
+
+			bool FileContentsAreEqual(string filePath1, string filePath2)
+			{
+				byte[] file1Bytes = File.ReadAllBytes(filePath1);
+				byte[] file2Bytes = File.ReadAllBytes(filePath2);
+
+				if (file1Bytes.Length != file2Bytes.Length)
+					return false;
+
+				for (int i = 0; i < file1Bytes.Length; i++)
+				{
+					if (file1Bytes[i] != file2Bytes[i])
+						return false;
+				}
+
+				return true;
+			}
+		}
+
+		private void CreateTestFilesAndFolders(string basePath)
+		{
+			Directory.CreateDirectory(basePath);
+
+			string subDir1 = Path.Combine(basePath, "SubDir1");
+			string subDir2 = Path.Combine(basePath, "SubDir2");
+			Directory.CreateDirectory(subDir1);
+			Directory.CreateDirectory(subDir2);
+
+			File.WriteAllText(Path.Combine(basePath, "file1.txt"), "Content of file1.");
+			File.WriteAllText(Path.Combine(basePath, "file2.txt"), "Content of file2.");
+
+			File.WriteAllText(Path.Combine(subDir1, "file3.txt"), "Content of file3 in SubDir1.");
+			File.WriteAllText(Path.Combine(subDir1, "file4.txt"), "Content of file4 in SubDir1.");
+
+			File.WriteAllText(Path.Combine(subDir2, "file5.txt"), "Content of file5 in SubDir2.");
+			File.WriteAllText(Path.Combine(subDir2, "file6.txt"), "Content of file6 in SubDir2.");
+		}
+
+		private void DeleteTestFilesAndFolders(string basePath)
+		{
+			if (Directory.Exists(basePath))
+			{
+				Directory.Delete(basePath, true);
 			}
 		}
 
 		[Fact]
 		public void TestTar()
 		{
-			string rootPath = Path.Combine(Path.GetTempPath(), "TestTar_" + Guid.NewGuid().ToString());
-			Directory.CreateDirectory(rootPath);
-
-			List<string> filePaths;
-			List<string> dirPaths;
-
-			CreateTestFiles(rootPath, out filePaths, out dirPaths);
-
-			try
-			{
-				List<string> itemsToCompress = new List<string>();
-				itemsToCompress.AddRange(filePaths);
-				itemsToCompress.AddRange(dirPaths);
-
-				string tarFilePath = Path.Combine(Path.GetTempPath(), "test.tar");
-
-				bool compressResult = GXCompressor.Compress(itemsToCompress, tarFilePath, -1, ref messages);
-				Assert.True(compressResult);
-
-				CleanupTestFiles(filePaths, dirPaths, rootPath);
-				Directory.CreateDirectory(rootPath);
-
-				bool decompressResult = GXCompressor.Decompress(tarFilePath, rootPath, ref messages);
-				Assert.True(decompressResult);
-
-				foreach (string filePath in filePaths)
-				{
-					Assert.True(File.Exists(filePath));
-				}
-
-				foreach (string dirPath in dirPaths)
-				{
-					Assert.True(Directory.Exists(dirPath));
-				}
-
-				CleanupTestFiles(filePaths, dirPaths, rootPath);
-
-				if (File.Exists(tarFilePath))
-				{
-					File.Delete(tarFilePath);
-				}
-			}
-			finally
-			{
-				if (Directory.Exists(rootPath))
-				{
-					Directory.Delete(rootPath, true);
-				}
-			}
 		}
 
 		[Fact]
 		public void TestGZip()
 		{
-			string rootPath = Path.Combine(Path.GetTempPath(), "TestGZip_" + Guid.NewGuid().ToString());
-			Directory.CreateDirectory(rootPath);
-
-			List<string> filePaths;
-			List<string> dirPaths;
-
-			string testFilePath = Path.Combine(rootPath, "testfile.txt");
-			File.WriteAllText(testFilePath, "This is a test file for GZip compression.");
-			filePaths = new List<string> { testFilePath };
-			dirPaths = new List<string>();
-
-			try
-			{
-				string gzipFilePath = Path.Combine(Path.GetTempPath(), "test.gz");
-
-				bool compressResult = GXCompressor.Compress(new List<string> { testFilePath }, gzipFilePath, 0, ref messages);
-				Assert.True(compressResult);
-
-				CleanupTestFiles(filePaths, dirPaths, rootPath);
-				Directory.CreateDirectory(rootPath);
-
-				bool decompressResult = GXCompressor.Decompress(gzipFilePath, rootPath, ref messages);
-				Assert.True(decompressResult);
-
-				Assert.True(File.Exists(testFilePath));
-
-				CleanupTestFiles(filePaths, dirPaths, rootPath);
-
-				if (File.Exists(gzipFilePath))
-				{
-					File.Delete(gzipFilePath);
-				}
-			}
-			finally
-			{
-				if (Directory.Exists(rootPath))
-				{
-					Directory.Delete(rootPath, true);
-				}
-			}
 		}
+
 
 		[Fact]
 		public void TestJar()
 		{
-			string rootPath = Path.Combine(Path.GetTempPath(), "TestJar_" + Guid.NewGuid().ToString());
-			Directory.CreateDirectory(rootPath);
-
-			List<string> filePaths;
-			List<string> dirPaths;
-
-			CreateTestFiles(rootPath, out filePaths, out dirPaths);
-
-			try
-			{
-				List<string> itemsToCompress = new List<string>();
-				itemsToCompress.AddRange(filePaths);
-				itemsToCompress.AddRange(dirPaths);
-
-				string jarFilePath = Path.Combine(Path.GetTempPath(), "test.jar");
-
-				bool compressResult = GXCompressor.Compress(itemsToCompress, jarFilePath, -1, ref messages);
-				Assert.True(compressResult);
-
-				CleanupTestFiles(filePaths, dirPaths, rootPath);
-				Directory.CreateDirectory(rootPath);
-
-				bool decompressResult = GXCompressor.Decompress(jarFilePath, rootPath, ref messages);
-				Assert.True(decompressResult);
-
-				foreach (string filePath in filePaths)
-				{
-					Assert.True(File.Exists(filePath));
-				}
-
-				foreach (string dirPath in dirPaths)
-				{
-					Assert.True(Directory.Exists(dirPath));
-				}
-
-				CleanupTestFiles(filePaths, dirPaths, rootPath);
-
-				if (File.Exists(jarFilePath))
-				{
-					File.Delete(jarFilePath);
-				}
-			}
-			finally
-			{
-				if (Directory.Exists(rootPath))
-				{
-					Directory.Delete(rootPath, true);
-				}
-			}
 		}
 
 		[Fact]
 		public void TestUnsupportedFormat()
 		{
-			string rootPath = Path.Combine(Path.GetTempPath(), "TestUnsupportedFormat_" + Guid.NewGuid().ToString());
-			Directory.CreateDirectory(rootPath);
-
-			List<string> filePaths;
-			List<string> dirPaths;
-
-			CreateTestFiles(rootPath, out filePaths, out dirPaths);
-
-			try
-			{
-				List<string> itemsToCompress = new List<string>();
-				itemsToCompress.AddRange(filePaths);
-				itemsToCompress.AddRange(dirPaths);
-
-				string unsupportedFilePath = Path.Combine(Path.GetTempPath(), "test.unsupported");
-
-				bool compressResult = GXCompressor.Compress(itemsToCompress, unsupportedFilePath, -1, ref messages);
-				Assert.False(compressResult);
-			}
-			finally
-			{
-				CleanupTestFiles(filePaths, dirPaths, rootPath);
-			}
 		}
+
 	}
+
 }
