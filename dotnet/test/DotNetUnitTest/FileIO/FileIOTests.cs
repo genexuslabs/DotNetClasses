1f2c62926ec8bc861eff2a074d889258213fa678
Claudia Murialdo <cmurialdo@genexus.com>
2024-12-23 19:42:08 -0300

    Try to fix failing test for DotNetCore. Failed UnitTesting.FileIOTests.GXDBFilePathTest System.UriFormatException : Invalid URI: The format of the URI could not be determined.

--- dotnet/test/DotNetUnitTest/FileIO/FileIOTests.cs
+++ dotnet/test/DotNetUnitTest/FileIO/FileIOTests.cs
@@ -11,7 +11,9 @@ namespace UnitTesting
 	{
 		public FileIOTests()
 		{
+#if !NETCORE
 			Config.ConfigFileName = Path.Combine(BaseDir, "client.exe.config");
+#endif
 		}
 		[Fact]
 		public void FileSharedToCopy()
@@ -74,8 +76,15 @@ namespace UnitTesting
 			{
 				string newFileName = GXDbFile.ResolveUri($"{GXDbFile.Scheme}:{fileName}", false);
 				string baseDir = Preferences.getBLOB_PATH();
-				bool isOK = new Uri(newFileName).LocalPath.StartsWith(Path.GetFullPath(baseDir), StringComparison.OrdinalIgnoreCase);
-				Assert.True(isOK);
+				try
+				{
+					bool isOK = new Uri(newFileName).LocalPath.StartsWith(Path.GetFullPath(baseDir), StringComparison.OrdinalIgnoreCase);
+					Assert.True(isOK);
+				}
+				catch (Exception ex)
+				{
+					Assert.True(false,  $"FileName:{newFileName} Error:{ex.Message}");
+				}
 			}
 		}
 
