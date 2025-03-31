47b770bd91d047469b55ec91aa435c9c406ec39b
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2025-03-31 14:13:59 +0000

    Cherry pick branch 'genexuslabs:update-sqlserver-and-drawing-dependencies' into beta

--- dotnet/src/dotnetframework/GxClasses/Core/GXUtilsCommon.cs
+++ dotnet/src/dotnetframework/GxClasses/Core/GXUtilsCommon.cs
@@ -6277,7 +6277,11 @@ namespace GeneXus.Utils
 						using (Graphics g = Graphics.FromImage(rotatedImage))
 						{
 							g.TranslateTransform(OriginalImage.Width / 2, OriginalImage.Height / 2);
-							g.RotateTransform(angle);
+#if NETCORE
+							g.RotateTransform(-angle); //TO DO bug reported to GeneXus.Commond.Drawing, it should be angle instead of -angle
+#else
+							g.RotateTransform(angle); //TO DO bug reported to GeneXus.Commond.Drawing, it should be angle instead of -angle
+#endif
 							g.TranslateTransform(-OriginalImage.Width / 2, -OriginalImage.Height / 2);
 							g.DrawImage(OriginalImage, new Point(0, 0));
 							modifiedImage = Save(rotatedImage, imageFile, GetImageFormat(OriginalImage));
