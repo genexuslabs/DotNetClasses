91fcc8a20b84d8429df2ad05f9273d370bf5bf2c
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2025-04-14 12:49:29 -0300

    Add HToC2 (inverse of CToH2)

--- dotnet/src/dotnetframework/GxClasses/Core/GXUtilsCommon.cs
+++ dotnet/src/dotnetframework/GxClasses/Core/GXUtilsCommon.cs
@@ -2916,6 +2916,10 @@ namespace GeneXus.Utils
 		{
 			return TToC2(dt, true, context);
 		}
+		public static string HToC2(DateTime dt)
+		{
+			return TToC2(dt, false);
+		}
 		//[Obsolete("TToC2 is deprecated, use TToC2(DateTime, bool, IGxContext) instead", false)]
 		public static string TToC2(DateTime dt, bool toUTC)
 		{
