fe6ed6c8307ea9528b9334d8bc89a5eebaef4844
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2025-04-23 10:11:52 -0300

    Relocate Kafka external object implementation to this repository (#1129)
    * Relocate Kafka external object implementation to this repository
    Issue:201487
    
    * Remove legacy code which was replaced with unit tests.
    
    * Remove unneeded Install Docker step.
    
    * Start docker service before docker compose
    
    * Migrate to ubuntu-latest
    
    * Fix docker compose command
    
    * Run only .net core tests on ubuntu-latest
    
    * Change directory separator
    
    * Unify unit test package versions
    
    * Add [SkippableFact]
    
    * Update README.md
    
    (cherry picked from commit 26e65796fbec2994fb9df2f429b1c3a54076de0f)

---
+++ dotnet/src/extensions/kafka/src/MessageResponse.cs
@@ -0,0 +1,88 @@
+﻿namespace GeneXus.Messaging.Core
+{
+	public class MessageResponse
+	{
+		public string Key { set; get; }
+		public string Value { set; get; }
+		public string Topic { set; get; }
+		public MessageResponseError Error { set; get; }
+	}
+
+	public class MessageResponseError
+	{
+		/// <summary>
+		///     Initialize a new Error instance from a particular
+		///     <see cref="ErrorCode"/> value.
+		/// </summary>
+		/// <param name="code">
+		///     The <see cref="ErrorCode"/> value associated with this Error.
+		/// </param>
+		/// <remarks>
+		///     The reason string associated with this Error will
+		///     be a static value associated with the <see cref="ErrorCode"/>.
+		/// </remarks>
+		/// 
+		public MessageResponseError()
+		{
+
+		}
+		public MessageResponseError(int code)
+		{
+			Code = code;
+			reason = null;
+		}
+
+		/// <summary>
+		///     Initialize a new Error instance from a particular
+		///     <see cref="ErrorCode"/> value and custom <paramref name="reason"/>
+		///     string.
+		/// </summary>
+		/// <param name="code">
+		///     The <see cref="ErrorCode"/> value associated with this Error.
+		/// </param>
+		/// <param name="reason">
+		///     A custom reason string associated with the error
+		///     (overriding the static string associated with 
+		///     <paramref name="code"/>).
+		/// </param>
+		public MessageResponseError(int code, string reason)
+		{
+			Code = code;
+			this.reason = reason;
+		}
+
+		/// <summary>
+		///     Gets the <see cref="ErrorCode"/> associated with this Error.
+		/// </summary>
+		public int Code { get; }
+
+		private string reason;
+
+		/// <summary>
+		///     Gets a human readable reason string associated with this error.
+		/// </summary>
+		public string Reason
+		{
+			get { return reason; }
+		}
+
+		/// <summary>
+		///     true if Code != ErrorCode.NoError.
+		/// </summary>
+		public bool HasError
+			=> Code != 0;
+
+		/// <summary>
+		///     true if this is error originated locally (within librdkafka), false otherwise.
+		/// </summary>
+		public bool IsLocalError
+			=> (int)Code < -1;
+
+		/// <summary>
+		///     true if this error originated on a broker, false otherwise.
+		/// </summary>
+		public bool IsBrokerError
+			=> (int)Code > 0;
+	
+	}
+}
