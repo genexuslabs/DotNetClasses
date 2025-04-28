2b59444b16bcc94f863ba4fb47248437f7164e81
sjuarezgx <sjuarez@genexus.com>
2025-04-28 19:26:52 -0300

    Refactor Azure Functions to use declarative REST services instead of manual routing (#1099)
    ---------
    
    Co-authored-by: Sabrina Juarez Garcia <sabrina.juarez@globant.com>

--- dotnet/src/extensions/Azure/Handlers/Program.cs
+++ dotnet/src/extensions/Azure/Handlers/Program.cs
@@ -1,6 +1,9 @@
+using System;
+using System.ComponentModel;
 using System.IO;
 using System.Reflection;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using GeneXus.Application;
 using GeneXus.Cache;
@@ -8,31 +11,37 @@ using GeneXus.Deploy.AzureFunctions.Handlers.Helpers;
 using GeneXus.Services;
 using GxClasses.Web;
 using GxClasses.Web.Middleware;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace GeneXus.Deploy.AzureFunctions.Handlers
 {
 	public class Program
-    {
+	{
 		static async Task Main()
-        {
+		{
 			string roothPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 			string routePrefix = GetRoutePrefix(roothPath);
 			GXRouting.ContentRootPath = roothPath;
 
 			var host = new HostBuilder()
-				.ConfigureFunctionsWorkerDefaults()
+				.ConfigureFunctionsWebApplication()
 				.ConfigureServices(services =>
 				{
+					services.AddMvc();
+					// Only needed if using HttpRequestData/HttpResponseData and a serializer that doesn't support asynchronous IO
+					services.Configure<KestrelServerOptions>(options => options.AllowSynchronousIO = true);
 					services.AddSingleton<ICallMappings, CallMappings>(x => new CallMappings(roothPath));
-				})
-				.ConfigureServices(services =>
-				{
 					services.AddSingleton<IGXRouting, GXRouting>(x => new GXRouting(routePrefix));
-				})
-				.ConfigureServices(services =>
-				{
+					services.AddControllers();
+					services.AddMvc().AddJsonOptions(options =>
+					{
+
+						options.JsonSerializerOptions.NumberHandling = System.Text.Json.Serialization.JsonNumberHandling.AllowReadingFromString;
+						options.JsonSerializerOptions.Converters.Add(new StringConverter());
+
+					});
 					ISessionService sessionService = GXSessionServiceFactory.GetProvider();
 					if (sessionService is GxRedisSession)
 					{
@@ -45,7 +54,29 @@ namespace GeneXus.Deploy.AzureFunctions.Handlers
 			GxContext.IsAzureContext = true;
 			GxContext.IsHttpContext = true;
 			await host.RunAsync();
-        }
+		}
+
+		public class StringConverter : JsonConverter<string>
+		{
+			public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+			{
+				if (reader.TokenType == JsonTokenType.Number)
+				{
+					if (reader.TryGetInt32(out int l))
+						return l.ToString();
+					else if (reader.TryGetDecimal(out decimal d))
+						return d.ToString();
+					else
+						return reader.GetDouble().ToString();
+				}
+				return reader.GetString();
+			}
+
+			public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+			{
+				writer.WriteStringValue(value);
+			}
+		}
 		private static string GetRoutePrefix(string ContentRootPath)
 		{
 			//Read host.json file to get Route prefix
