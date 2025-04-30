4eaf4d6794a59852a04159eddb70c16d136bc5a3
Alejandro Panizza Carve <apanizza@genexus.com>
2025-04-30 16:36:12 -0300

    - Add header methods in RESTApi classes. (#1101)
    - Added methods for header parameters in client for remote REST call

--- dotnet/src/dotnetframework/GxClasses/Domain/GxHttpClient.cs
+++ dotnet/src/dotnetframework/GxClasses/Domain/GxHttpClient.cs
@@ -21,6 +21,7 @@ namespace GeneXus.Http.Client
 	using GeneXus.Application;
 	using GeneXus.Configuration;
 	using GeneXus.Utils;
+
 #if NETCORE
 	using Microsoft.AspNetCore.WebUtilities;
 #endif
@@ -2002,6 +2003,10 @@ namespace GeneXus.Http.Client
 		{
 			value = Convert.ToDouble(GetHeader(name));
 		}
+		public void GetHeader(string name, out Decimal value)
+		{
+			value = Convert.ToDecimal(GetHeader(name));
+		}
 		public void GetHeader(string name, out string value)
 		{
 			value = GetHeader(name);
@@ -2009,6 +2014,10 @@ namespace GeneXus.Http.Client
 		public void GetHeader(string name, out DateTime value)
 		{
 			value = Convert.ToDateTime(GetHeader(name));
+		}
+		public void GetHeader(string name, out bool value)
+		{
+			value = Convert.ToBoolean(GetHeader(name));
 		}
 		public void AddCertificate(string cert)
 		{
