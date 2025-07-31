0c083dd9cab4c37f39566745f488d9358dea450f
claudiamurialdo <c.murialdo@globant.com>
2025-07-31 12:28:31 +0000

    Cherry pick branch 'genexuslabs:gamsaml20_eo' into beta

--- dotnet/src/extensions/gam/src/DotNetFramework/GamSaml20/Properties/AssemblyInfo.cs
+++ dotnet/src/extensions/gam/src/DotNetFramework/GamSaml20/Properties/AssemblyInfo.cs
@@ -7,4 +7,8 @@ using System.Security;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AllowPartiallyTrustedCallers]
+#if GX_SIGNED_ASSEMBLY
 [assembly: InternalsVisibleTo("GamTest, PublicKey=0024000004800000940000000602000000240000525341310004000001000100e52e348955bc6a30ec9c7319f94e2ec18fd73d4445273f81bd2132e7309b52f3582b72d9dcd0231806e6eea947b40d948767cd8bdc33b738e2787bef3d0c8200979748684fe3aa4a39adc2e81e05750291dde1b82362d82dc0e611c6048d51483f67bd656f21db0a70679b30ec744b3e66d518d25b66f66be72674f664c58c97")]
+#else
+[assembly: InternalsVisibleTo("GamTest")]
+#endif
