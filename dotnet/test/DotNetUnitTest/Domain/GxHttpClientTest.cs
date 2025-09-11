383cafc6831602ab6940e17bb3812365bd3cd5e7
claudiamurialdo <c.murialdo@globant.com>
2025-09-11 15:34:10 +0000

    Cherry pick branch 'genexuslabs:fix/local-test' into beta

--- dotnet/test/DotNetUnitTest/Domain/GxHttpClientTest.cs
+++ dotnet/test/DotNetUnitTest/Domain/GxHttpClientTest.cs
@@ -45,7 +45,7 @@ namespace xUnitTesting
 			}
 		}
 
-		[Fact]
+		[Fact(Skip ="For Local Test")]
 		public void HttpClientInvalidURLWithCustomPort()
 		{
 			using (GxHttpClient httpclient = new GxHttpClient())
