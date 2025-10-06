85e91509851cb77860b5254dcc7e0f05f830f8f0
claudiamurialdo <c.murialdo@globant.com>
2025-10-06 14:05:32 +0000

    Cherry pick branch 'genexuslabs:fix/httpclient-to-file-relative' into beta

--- dotnet/src/dotnetframework/GxClasses/Domain/GxHttpClient.cs
+++ dotnet/src/dotnetframework/GxClasses/Domain/GxHttpClient.cs
@@ -2027,7 +2027,7 @@ namespace GeneXus.Http.Client
 #if !NETCORE
 			if (HttpContext.Current != null)
 #endif
-			if (fileName.IndexOfAny(new char[] { '\\', ':' }) == -1)
+			if (!Path.IsPathRooted(fileName))
 				pathName = Path.Combine(GxContext.StaticPhysicalPath(), fileName);
 
 			if (ReceiveData != null)
