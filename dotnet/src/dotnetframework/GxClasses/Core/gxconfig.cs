23931cbf657e3d7af6304addbfe1c043118eae19
claudiamurialdo <c.murialdo@globant.com>
2025-10-10 12:30:09 -0300

    Rename constant HTTPCLIENT_USER_AGENT to UserAgentHeader for consistency with the property name

--- dotnet/src/dotnetframework/GxClasses/Core/gxconfig.cs
+++ dotnet/src/dotnetframework/GxClasses/Core/gxconfig.cs
@@ -860,7 +860,7 @@ namespace GeneXus.Configuration
 		public static string DefaultRewriteFile = "rewrite.config";
 		const string USE_NAMED_PARAMETERS = "UseNamedParameters";
 		const string REST_DATES_WITH_MILLIS = "REST_DATES_WITH_MILLIS";
-		const string HTTPCLIENT_USER_AGENT = "HTTPCLIENT_USER_AGENT";
+		const string UserAgentHeader = "UserAgentHeader";
 		internal const string YES = "1";
 		internal const string NO = "0";
 		static string defaultDatastore;
@@ -880,7 +880,7 @@ namespace GeneXus.Configuration
 		{
 			get
 			{
-				if (Config.GetValueOrEnvironmentVarOf(HTTPCLIENT_USER_AGENT, out string userAgent))
+				if (Config.GetValueOrEnvironmentVarOf(UserAgentHeader, out string userAgent))
 					return userAgent;
 				else
 					return string.Empty;
