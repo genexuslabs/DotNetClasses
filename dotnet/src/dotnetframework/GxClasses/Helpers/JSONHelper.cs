1116b1cdb6c7320713f670412daeddbaa3d56296
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2025-10-16 21:52:19 -0300

    ChatMessages parameter in Agent.Call was not accumulating messages (#1212)
    * Apply fix on #1208 to CallAgent
    
    * Add default ToJson implementation for ExternalCollections, enabling proper ToJson behavior for ChatMessage collections.
    
    * Remove identation.
    
    ---------
    
    Co-authored-by: claudiamurialdo <c.murialdo@globant.com>

--- dotnet/src/dotnetframework/GxClasses/Helpers/JSONHelper.cs
+++ dotnet/src/dotnetframework/GxClasses/Helpers/JSONHelper.cs
@@ -128,6 +128,15 @@ namespace GeneXus.Utils
 			});
 		}
 
+		internal override string DefaultSerialize<T>(T value)
+		{
+			if (value != null)
+			{
+				return JsonSerializer.Serialize(value);
+			}
+			return null;
+		}
+
 		internal override bool IsJsonString(string jobject)
 		{
 			try
@@ -229,6 +238,9 @@ namespace GeneXus.Utils
 		internal abstract string WriteJSON<T>(T kbObject) where T : class;
 
 		internal abstract string WriteNullableJSON(Dictionary<string, object> kbObject);
+#if NETCORE
+		internal abstract string DefaultSerialize<T>(T value) where T : class;
+#endif
 	}
 	
 	public class JSONHelper
@@ -328,7 +340,20 @@ namespace GeneXus.Utils
 		{
 			return Serialize<T>(kbObject, Encoding.UTF8);
 		}
-
+#if NETCORE
+		public static string DefaultSerialize<T>(T value) where T : class
+		{
+			try
+			{
+				return GXJsonSerializer.Instance.DefaultSerialize<T>(value);
+			}
+			catch (Exception ex)
+			{
+				GXLogging.Error(log, "DefaultSerialize error ", ex);
+				return null;
+			}
+		}
+#endif
 		public static string Serialize<T>(T kbObject, Encoding encoding) where T : class
 		{
 			return Serialize<T>(kbObject, encoding, null);
