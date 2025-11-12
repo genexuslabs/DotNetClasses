0bae406b7ac07c82ec7097abe7cac23d12f6c453
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2025-11-12 12:35:10 -0300

    Add route attribute to HomeController for default route handling (#1223)
    Co-authored-by: claudiamurialdo <c.murialdo@globant.com>
    (cherry picked from commit 70bdb40ffb10ba1a4045ceb29f81fc9349b3d3c8)

--- dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
+++ dotnet/src/dotnetcore/GxNetCoreStartup/Startup.cs
@@ -802,6 +802,7 @@ namespace GeneXus.Application
 			return builder.UseMiddleware<EnableRequestRewindMiddleware>();
 		}
 	}
+	[Route("")]
 	public class HomeController : Controller
 	{
 		public IActionResult Index()
