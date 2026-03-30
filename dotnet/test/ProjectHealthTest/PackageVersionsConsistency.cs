52314bb7d018652cc269c1f2a2fee8fab18254fd
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2026-03-30 14:20:38 -0300

    Apply changes suggested by azzato

--- dotnet/test/ProjectHealthTest/PackageVersionsConsistency.cs
+++ dotnet/test/ProjectHealthTest/PackageVersionsConsistency.cs
@@ -19,7 +19,7 @@ namespace ProjectHealthTest
 		private const string PACKAGE_VERSION_ATTRIBUTE_NAME = "Version";
 		private const string TARGET_FRAMEWORK = "Project/PropertyGroup/TargetFramework";
 		private const string TARGET_FRAMEWORKS = "Project/PropertyGroup/TargetFrameworks";
-		private const string NET8 = "net8.0";
+		private const string NET10 = "net10.0";
 		private const string NET_FRAMEWORK = "net462";
 		private static HashSet<string> ExcludedFromTransitiveDependenciesControl = new HashSet<string> {"runtime"};
 		private static HashSet<string> ProjectTemporaryExcludedFromDependenciesControl = new HashSet<string> { "GeneXus.Deploy.AzureFunctions.Handlers.csproj", "AzureFunctionsTest.csproj" };
@@ -35,7 +35,7 @@ namespace ProjectHealthTest
 		[Fact]
 		public void TestPackageVersionConsistencyAcrossNETProjectsAndTransitives()
 		{
-			TestPackageVersionConsistencyAcrossProjects(NET8, true);
+			TestPackageVersionConsistencyAcrossProjects(NET10, true);
 		}
 		/// <summary>
 		/// Tests that all referenced packages have the same version by doing:
@@ -47,7 +47,7 @@ namespace ProjectHealthTest
 		[Fact]
 		public void TestPackageVersionConsistencyAcrossNETProjects()
 		{
-			TestPackageVersionConsistencyAcrossProjects(NET8, false);
+			TestPackageVersionConsistencyAcrossProjects(NET10, false);
 		}
 		[Fact]
 		public void TestPackageVersionConsistencyAcrossNETFrameworkProjects()
@@ -100,15 +100,15 @@ namespace ProjectHealthTest
 						{
 							XmlAttribute condition = packageNode.ParentNode.Attributes["Condition"];
 							if (condition != null) {
-								if (targetFramework == NET8 && condition.Value.Contains($"=='{NET_FRAMEWORK}'", StringComparison.OrdinalIgnoreCase))
+								if (targetFramework == NET10 && condition.Value.Contains($"=='{NET_FRAMEWORK}'", StringComparison.OrdinalIgnoreCase))
 								{
 									continue;
 								}
-								else if (targetFramework == NET8 && condition.Value.Contains($"!='{NET8}'", StringComparison.OrdinalIgnoreCase))
+								else if (targetFramework == NET10 && condition.Value.Contains($"!='{NET10}'", StringComparison.OrdinalIgnoreCase))
 								{
 									continue;
 								}
-								else if (targetFramework == NET_FRAMEWORK && condition.Value.Contains($"=='{NET8}'", StringComparison.OrdinalIgnoreCase))
+								else if (targetFramework == NET_FRAMEWORK && condition.Value.Contains($"=='{NET10}'", StringComparison.OrdinalIgnoreCase))
 								{
 									continue;
 								}
